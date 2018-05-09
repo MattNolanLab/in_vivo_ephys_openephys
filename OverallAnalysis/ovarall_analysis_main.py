db6372d5676b905631f0c209aa8f4b2a6e13daca
Klara Gerlei <klarizsofi@gmail.com>
2018-05-09 14:00:05 +0100

    get overall csv into panda frame

--- OverallAnalysis/ovarall_analysis_main.py
+++ OverallAnalysis/ovarall_analysis_main.py
@@ -1,22 +1,19 @@
 #  This script is to perform overall analysis on multiple days recorded from a group of animals
 import OverallAnalysis.organize_cluster_data
+import pandas as pd
 
-path_to_data = 'C:/Users/s1466507/Desktop'
-
-
-def construct_data_frame():
-    OverallAnalysis.organize_cluster_data.get_sorting_output()
-    pass
+path_to_data = 'C:/Users/s1466507/Desktop/data_all.csv'
 
 
 def run_analyses():
+    spike_data_frame = pd.read_csv(path_to_data)  # reads csv, puts it in df
+    print(spike_data_frame.head())
     pass
 
 
 def main():
     print('-------------------------------------------------------------')
     print('-------------------------------------------------------------')
-    construct_data_frame()
     run_analyses()
 
 
