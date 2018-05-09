b4c2e71d826225d728016033dbd172a924d40179
Klara Gerlei <klarizsofi@gmail.com>
2018-05-09 17:10:24 +0100

    get overall csv into panda frame

--- OverallAnalysis/ovarall_analysis_main.py
+++ OverallAnalysis/ovarall_analysis_main.py
@@ -1,13 +1,21 @@
 #  This script is to perform overall analysis on multiple days recorded from a group of animals
 import OverallAnalysis.organize_cluster_data
 import pandas as pd
+import scipy.io
 
-path_to_data = 'C:/Users/s1466507/Desktop/data_all.csv'
+path_to_data = 'C:/Users/s1466507/Documents/Ephys/test_overall_analysis/'
+
+
+def get_snippets(filename):
+    snippets = scipy.io.loadmat(path_to_data + '/' + filename + 'Firings0.mat')
+    return snippets
 
 
 def run_analyses():
-    spike_data_frame = pd.read_csv(path_to_data)  # reads csv, puts it in df
+    spike_data_frame = pd.read_csv(path_to_data + 'data_all.csv')  # reads csv, puts it in df
     print(spike_data_frame.head())
+    #snippets = get_snippets(path_to_data + )
+
     pass
 
 
