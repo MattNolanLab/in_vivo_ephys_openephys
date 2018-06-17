b29aab59ceed52c28d5226f0fd0f86827b076a39
Klara Gerlei <klarizsofi@gmail.com>
2018-06-17 14:57:47 +0100

    read bonsai output into dataframe
    dataframe is created from csv.
    (1) columns are read into df using ' ' space as a delimiter, which creates an extra line of 'NaN's due to a space at the end of each line. This extra line is removed
    (2) the first column is split into date and time usinf 'T' as a delimiter
    (3) columns are named
    x_left
    y_left
    x_right
    y_right
    syncLED
    data
    time
    
    Each row is one sampling point.

--- PostSorting/open_field_get_spatial_data.py
+++ PostSorting/open_field_get_spatial_data.py
@@ -1,6 +1,7 @@
 import csv
 import glob
 import os
+import pandas as pd
 
 ''' The name of bonsai output files is not standardised in all experiments, so this function checks all csv
 files in the recording folder and reads the first line. Our bonsai output files start with the date and 'T'
@@ -27,15 +28,18 @@ def find_bonsai_file(recording_folder):
                 except Exception as ex:
                     print('Could not read csv file:')
                     print(name)
+                    print(ex)
 
     return path_to_bonsai_file, is_found
 
 
 def read_position(path_to_bonsai_file):
-    position_data = []
-    # read csv file and return
-    # time and xy coordinates for the two beads tracked
-    # could be dataframe with time as index 'position_data'
+    position_data = pd.read_csv(path_to_bonsai_file, sep=' ', header=None)
+    if len(list(position_data)) > 6:
+        position_data = position_data.drop([6], axis=1)  # remove column of NaNs due to extra space at end of lines
+    position_data['date'], position_data['time'] = position_data[0].str.split('T', 1).str
+    position_data = position_data.drop([0], axis=1) # remove first column that got split into date and time
+    position_data.columns = ['x_left', 'y_left', 'x_right', 'y_right', 'syncLED', 'date', 'time']
     return position_data
 
 
@@ -53,7 +57,7 @@ def calculate_head_direction(position_data):
 
 def process_position_data(recording_folder):
     path_to_bonsai_file, is_found = find_bonsai_file(recording_folder)
-    position_data = read_position(recording_folder + path_to_bonsai_file)
+    position_data = read_position(path_to_bonsai_file)
     position_of_mouse = calculate_position(position_data)
     head_direction_of_mosue = calculate_head_direction(position_data)
     # put these in session dataframe
@@ -64,6 +68,7 @@ def main():
     print('-------------------------------------------------------------')
 
     recording_folder = 'C:/Users/s1466507/Documents/Ephys/test_overall_analysis/M5_2018-03-06_15-34-44_of'
+    # recording_folder = 'C:/Users/s1466507/Documents/Ephys/test_overall_analysis/M0_2017-11-21_15-52-53'
     process_position_data(recording_folder)
 
 
