891165c18fc267cae9894ee05c4966411cc3dfe8
Klara Gerlei <klarizsofi@gmail.com>
2018-06-28 23:19:14 +0100

    progress on spatial firing
    still not perfect

--- PostSorting/open_field_spatial_firing.py
+++ PostSorting/open_field_spatial_firing.py
@@ -12,17 +12,25 @@ def find_firing_location_indices2(spike_data, spatial_data):
 
 
 def find_firing_location_indices(spike_data, spatial_data):
-    pass
+    for cluster in range(len(spike_data)):
+        bonsai_indices_cluster = spike_data.bonsai_indices[cluster]
+        bonsai_indices_cluster_round = bonsai_indices_cluster.round(0)
+        spike_data = spike_data.append({
+            "position_x": spatial_data.position_x[bonsai_indices_cluster_round], # this is still bad - maybe when I removed the beginning of the synced array it didnt change indices
+            "position_y":  spatial_data.position_y[bonsai_indices_cluster_round],
+            "hd": spatial_data.hd[bonsai_indices_cluster_round]
+        }, ignore_index=True)
+    return spike_data
 
 
 
 def add_firing_locations(spike_data, spatial_data):
     # convert firing times to seconds
-    avg_sampling_rate_bonsai = float(1 / spatial_data['time_seconds'].diff().mean())
+    avg_sampling_rate_bonsai = float(1 / spatial_data['synced_time'].diff().mean())
     avg_sampling_rate_open_ephys = 30000
     sampling_rate_rate = avg_sampling_rate_open_ephys/avg_sampling_rate_bonsai
-    # spike_data['bonsai_indices'] = int(spike_data.index/sampling_rate_rate) # not good. should be based on max firing
-    find_firing_location_indices(spike_data, spatial_data)
+    spike_data['bonsai_indices'] = spike_data.firing_times/sampling_rate_rate
+    spike_data = find_firing_location_indices(spike_data, spatial_data)
 
     # find closest times in spatial data
     # get the corresponding xy values
