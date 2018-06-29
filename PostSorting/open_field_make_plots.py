84aabcc790b3677904745783e9ffd922270bef23
Klara Gerlei <klarizsofi@gmail.com>
2018-06-29 15:12:28 +0100

    made test plot for spikes on trajectory

--- PostSorting/open_field_make_plots.py
+++ PostSorting/open_field_make_plots.py
@@ -7,9 +7,28 @@ def plot_position(position_data):
 
 
 def plot_spikes_on_trajectory(position_data, spike_data):
-    plt.plot(position_data['position_x'], position_data['position_y'], color='black', linewidth=10)
-    cluster_id = 0
-    plt.scatter(spike_data.position_x[0], spike_data.position_y[0], color='red', marker='o', s=30)
-    plt.show()
-    print('test')
+    cluster_id = 5  # this is just a test plot, it plots cluster 5
+    spikes_on_track = plt.figure()
+    ax = spikes_on_track.add_subplot(1, 1, 1)  # specify (nrows, ncols, axnum)
+    ax.plot(position_data['position_x'], position_data['position_y'], color='black', linewidth=2, zorder=1, alpha=0.7)
+    ax.scatter(spike_data.position_x[cluster_id], spike_data.position_y[cluster_id], color='red', marker='o', s=10, zorder=2)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.set_xlim([0, 450])
+    ax.set_ylim([0, 500])
+    plt.tick_params(
+        axis='both',          # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        bottom=False,      # ticks along the bottom edge are off
+        top=False,         # ticks along the top edge are off
+        right=False,
+        left=False,
+        labelleft=False,
+        labelbottom=False) # labels along the bottom edge are off
+    ax.set_aspect('equal')
+
+    plt.savefig('C:/Users/s1466507/Documents/Ephys/overall_figures/' + 'spatial_firing.png')
+
 
