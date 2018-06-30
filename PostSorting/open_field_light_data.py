3204652bad14fa76d36efd192be899269052c98c
Klara Gerlei <klarizsofi@gmail.com>
2018-06-30 13:48:52 +0100

    remove spatial data during opto tagging

--- PostSorting/open_field_light_data.py
+++ PostSorting/open_field_light_data.py
@@ -2,6 +2,8 @@ import open_ephys_IO
 import os
 import numpy as np
 
+import PostSorting.open_field_make_plots
+
 
 def load_opto_data(recording_to_process, prm):
     is_found = False
@@ -30,3 +32,15 @@ def process_opto_data(recording_to_process, prm):
 
     return opto_on, opto_off, is_found
 
+
+# find first opto pulse and remove spatial data from that point
+def remove_spatial_data_during_opto_stimulation(opto_on, spatial_data):
+    first_opto_pulse_index = min(opto_on[0])
+    avg_sampling_rate_bonsai = float(1 / spatial_data['synced_time'].diff().mean())
+    avg_sampling_rate_open_ephys = 30000  # Hz
+    sampling_rate_rate = avg_sampling_rate_open_ephys/avg_sampling_rate_bonsai
+    first_opto_pulse_index_bonsai = first_opto_pulse_index/sampling_rate_rate
+    indices_to_remove = list(spatial_data.index[int(first_opto_pulse_index_bonsai):].values)
+    spatial_data = spatial_data.drop(indices_to_remove, axis=0)
+    PostSorting.open_field_make_plots.plot_position(spatial_data)
+    return spatial_data
