bc052c2474a61971b726bd89b36a750ffce5ab5b
Klara Gerlei <klarizsofi@gmail.com>
2018-07-12 12:40:26 +0100

    fixed ratemap for one cluster

--- PostSorting/open_field_firing_maps.py
+++ PostSorting/open_field_firing_maps.py
@@ -40,57 +40,60 @@ def get_spike_heatmaps2(firing_data_spatial):
 
 def get_dwell(spatial_data, prm):
     min_dwell_distance_cm = 5  # from point to determine min dwell time
-    min_dwell_distance_cm = min_dwell_distance_cm / 100 * prm.get_pixel_ratio()
+    min_dwell_distance_pixels = min_dwell_distance_cm / 100 * prm.get_pixel_ratio()
 
     dt_position_ms = spatial_data.synced_time.diff().mean()*1000  # average sampling interval in position data (ms)
     min_dwell_time_ms = 3 * dt_position_ms  # this is about 100 ms
     min_dwell = round(min_dwell_time_ms/dt_position_ms)
-    return min_dwell, min_dwell_distance_cm
+    return min_dwell, min_dwell_distance_pixels
 
 
 def get_bin_size(prm):
     bin_size_cm = 2.5
-    bin_size_cm = bin_size_cm / 100 * prm.get_pixel_ratio()
-    return bin_size_cm
+    bin_size_pixels = bin_size_cm / 100 * prm.get_pixel_ratio()
+    return bin_size_pixels
 
 
 def get_number_of_bins(spatial_data, prm):
-    bin_size_cm = get_bin_size(prm)
-    length_of_arena_x = spatial_data.position_x.max()
-    length_of_arena_y = spatial_data.position_y.max()
-    number_of_bins_x = math.ceil(length_of_arena_x / bin_size_cm)
-    number_of_bins_y = math.ceil(length_of_arena_y / bin_size_cm)
+    bin_size_pixels = get_bin_size(prm)
+    length_of_arena_x = spatial_data.position_x_pixels.max()
+    length_of_arena_y = spatial_data.position_y_pixels.max()
+    number_of_bins_x = math.ceil(length_of_arena_x / bin_size_pixels)
+    number_of_bins_y = math.ceil(length_of_arena_y / bin_size_pixels)
     return number_of_bins_x, number_of_bins_y
 
 
 def gaussian_kernel(kernx):
-    kerny = np.exp(np.power(-kernx, 2)/2)
+    kerny = np.exp(np.power(kernx, 2)/2 * (-1))
     return kerny
 
 
 def get_spike_heatmap(spatial_data, firing_data_spatial, prm):
     dt_position_ms = spatial_data.synced_time.diff().mean()*1000
-    min_dwell, min_dwell_distance_cm = get_dwell(spatial_data, prm)
+    min_dwell, min_dwell_distance_pixels = get_dwell(spatial_data, prm)
     smooth = 5 / 100 * prm.get_pixel_ratio()
-    bin_size_cm = get_bin_size(prm)
+    bin_size_pixels = get_bin_size(prm)
     number_of_bins_x, number_of_bins_y = get_number_of_bins(spatial_data, prm)
-    firing_rate_map = np.zeros((number_of_bins_x, number_of_bins_y))
-    for cluster in range(len(firing_data_spatial)):
-        cluster_firings = pd.DataFrame({'position_x': firing_data_spatial.position_x[cluster], 'position_y': firing_data_spatial.position_y[cluster]})
 
+    for cluster in range(len(firing_data_spatial)):
+        cluster_firings = pd.DataFrame({'position_x': firing_data_spatial.position_x_pixels[cluster], 'position_y': firing_data_spatial.position_y_pixels[cluster]})
+        firing_rate_map = np.zeros((number_of_bins_x, number_of_bins_y))
         for x in range(number_of_bins_x):
             for y in range(number_of_bins_y):
-                px = x * bin_size_cm - (bin_size_cm / 2)
-                py = y * bin_size_cm - (bin_size_cm / 2)
+                px = x * bin_size_pixels + (bin_size_pixels / 2)
+                py = y * bin_size_pixels + (bin_size_pixels / 2)
                 spike_distances = np.sqrt(np.power(px - cluster_firings.position_x.values, 2) + np.power(py - cluster_firings.position_y.values, 2))
-                occupancy_distances = np.sqrt(np.power((px - spatial_data.position_x.values), 2) + np.power((py - spatial_data.position_y.values), 2))
-                bin_occupancy = len(np.where(occupancy_distances < min_dwell_distance_cm)[0])
+                occupancy_distances = np.sqrt(np.power((px - spatial_data.position_x_pixels.values), 2) + np.power((py - spatial_data.position_y_pixels.values), 2))
+                bin_occupancy = len(np.where(occupancy_distances < min_dwell_distance_pixels)[0])
 
                 if bin_occupancy >= min_dwell:
-                    firing_rate_map[x, y] = sum(gaussian_kernel(spike_distances/smooth)) / (sum(gaussian_kernel(occupancy_distances/smooth)) * (dt_position_ms/1000))
+                    firing_rate_map[x, y] = sum(gaussian_kernel(spike_distances/smooth)) / (sum(gaussian_kernel(occupancy_distances/smooth))  * (dt_position_ms/1000))
+
                 else:
-                    firing_rate_map[x, y] = None
+                    firing_rate_map[x, y] = 0
+        np.savetxt(str(cluster) + "firingrate_python.csv", firing_rate_map, delimiter=",")
         plt.imshow(firing_rate_map, cmap='jet', interpolation='nearest')
+    # datadeame of firing rate maps needs to be constructed here todo
     return firing_rate_map
 
 
