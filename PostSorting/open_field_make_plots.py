c7d06930ba5fc90a729d4d5092e713100395ed8e
Klara Gerlei <klarizsofi@gmail.com>
2018-08-08 16:43:33 +0100

    plot firing field with markers on top

--- PostSorting/open_field_make_plots.py
+++ PostSorting/open_field_make_plots.py
@@ -4,7 +4,13 @@ import os
 import plot_utility
 import math
 import numpy as np
+import random
+
 from scipy.interpolate import spline
+import PostSorting.parameters
+
+import pandas as pd
+import PostSorting.open_field_firing_fields
 
 
 def plot_position(position_data):
@@ -91,3 +97,35 @@ def plot_polar_head_direction_histogram(hd_hist, spatial_firing, prm):
         ax.plot(theta[:-1], hd_hist*(max(hd_hist_cluster)/max(hd_hist)), color='black', linewidth=2)
         plt.savefig(save_path + '/' + spatial_firing.session_id[cluster] + '_hd_polar_' + str(cluster + 1) + '.png')
         plt.close()
+
+
+def plot_hd_for_firing_fields(spatial_firing, prm):
+    print('I will make the polar HD plots for individual firing fields now.')
+    save_path = prm.get_local_recording_folder_path() + '/Figures/firing_field_plots'
+    if os.path.exists(save_path) is False:
+        os.makedirs(save_path)
+    for cluster in range(len(spatial_firing)):
+        firing_rate_map = spatial_firing.firing_maps[cluster]
+        firing_rate_map_fig = plt.figure()
+        ax = firing_rate_map_fig.add_subplot(1, 1, 1)
+        ax.imshow(firing_rate_map)
+
+        if len(spatial_firing.firing_fields[cluster]) > 0:
+            firing_fields_cluster = spatial_firing.firing_fields[cluster]
+            colors = ["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)]) for i in range(len(firing_fields_cluster))]
+            for field_id, field in enumerate(firing_fields_cluster):
+                for bin in field:
+                    plt.scatter(bin[1], bin[0], color=colors[field_id], marker='o', s=20)
+
+
+def main():
+    prm = PostSorting.parameters.Parameters()
+    prm.set_local_recording_folder_path('C:/Users/s1466507/Documents/Ephys/test_overall_analysis/M5_2018-03-06_15-34-44_of/')
+    firing_rate_maps = np.load('C:/Users/s1466507/Documents/Ephys/test_overall_analysis/M5_2018-03-06_15-34-44_of/M5_2018-03-06_15-34-44_of.npy')
+    cluster_id = np.arange(len(firing_rate_maps))
+    spatial_firing = pd.DataFrame(cluster_id)
+    spatial_firing['firing_maps'] = list(firing_rate_maps)
+    spatial_firing = PostSorting.open_field_firing_fields.analyze_firing_fields(spatial_firing)
+    plot_hd_for_firing_fields(spatial_firing, prm)
+if __name__ == '__main__':
+    main()
