281aacc2bc9f5594fb2d9f6bf2e12b7b8ae95a96
Klara Gerlei <klarizsofi@gmail.com>
2018-08-12 13:12:31 +0100

    rotated plots by 90 degrees

--- PostSorting/open_field_firing_maps.py
+++ PostSorting/open_field_firing_maps.py
@@ -61,6 +61,7 @@ def calculate_firing_rate_for_cluster_parallel(cluster, smooth, firing_data_spat
 
             else:
                 firing_rate_map[x, y] = 0
+    firing_rate_map = np.rot90(firing_rate_map)
     return firing_rate_map
 
 
@@ -112,5 +113,6 @@ def get_position_heatmap(spatial_data, prm):
 def make_firing_field_maps(spatial_data, firing_data_spatial, prm):
     position_heat_map = get_position_heatmap(spatial_data, prm)
     firing_data_spatial = get_spike_heatmap_parallel(spatial_data, firing_data_spatial, prm)
-
+    position_heat_map = np.rot90(position_heat_map)  # to rotate map to be like matlab plots
+    plt.imshow(position_heat_map, cmap='jet', interpolation='nearest')
     return position_heat_map, firing_data_spatial
