bc1ffdf78b039f76f2c63904d0c2a4e87c4918a2
Klara Gerlei <klarizsofi@gmail.com>
2018-08-17 15:07:15 +0100

    added Sarah's functions to plot utility

--- plot_utility.py
+++ plot_utility.py
@@ -83,3 +83,69 @@ def generate_new_color(existing_colors, pastel_factor=0.5):
             max_distance = best_distance
             best_color = color
     return best_color
+
+
+def style_vr_plot(ax, x_max):
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(True)
+    ax.spines['bottom'].set_visible(True)
+    plt.tick_params(
+        axis='both',  # changes apply to the x-axis
+        which='both',  # both major and minor ticks are affected
+        bottom=True,  # ticks along the bottom edge are off
+        top=False,  # ticks along the top edge are off
+        right=False,
+        left=True,
+        labelleft=True,
+        labelbottom=True)  # labels along the bottom edge are off
+
+    #ax.set_aspect('equal')
+
+    ax.axvline(0, linewidth = 3, color = 'black') # bold line on the y axis
+    ax.axhline(0, linewidth = 3, color = 'black') # bold line on the x axis
+    plt.ylim(0, x_max)
+
+    return ax
+
+
+def style_track_plot(ax):
+    ax.axvspan(88, 88+22, facecolor='DarkGreen', alpha=.25, linewidth =0)
+    ax.axvspan(0, 30, facecolor='k', linewidth =0, alpha=.25) # black box
+    ax.axvspan(200-30, 200, facecolor='k', linewidth =0, alpha=.25)# black box
+
+
+def makelegend(fig,ax, x_location):
+    handles, labels = ax.get_legend_handles_labels()
+    leg = fig.legend(handles,labels, loc="upper right", bbox_to_anchor=(0.989, x_location), fontsize = "xx-large")
+    for l in leg.get_lines():l.set_linewidth(2)
+    frame = leg.get_frame()
+    frame.set_edgecolor('w')
+    frame.set_alpha(0.2)
+
+
+def adjust_spine_thickness(ax):
+    for axis in ['left','bottom']:
+        ax.spines[axis].set_linewidth(2)
+
+
+def adjust_spines(ax,spines):
+    for loc, spine in ax.spines.items():
+        if loc in spines:
+            spine.set_position(('outward',0)) # outward by 10 points
+            #spine.set_smart_bounds(True)
+        else:
+            spine.set_color('none') # don't draw spine
+
+    # turn off ticks where there is no spine
+    if 'left' in spines:
+        ax.yaxis.set_ticks_position('left')
+    else:
+        # no yaxis ticks
+        ax.yaxis.set_ticks([])
+
+    if 'bottom' in spines:
+        ax.xaxis.set_ticks_position('bottom')
+    else:
+        # no xaxis ticks
+        ax.xaxis.set_ticks([])
