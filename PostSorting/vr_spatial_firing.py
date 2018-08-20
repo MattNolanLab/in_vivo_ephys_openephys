d3209be64bd69cb1360f963087acb0991876db4f
sarahtennant <stennan2@exseed.ed.ac.uk>
2018-08-20 18:54:13 +0100

    sped up find_firing_location_indices

--- PostSorting/vr_spatial_firing.py
+++ PostSorting/vr_spatial_firing.py
@@ -6,21 +6,16 @@ prm = PostSorting.parameters.Parameters()
 
 def find_firing_location_indices(spike_data, spatial_data):
     print('I am extracting firing locations...')
-    spatial_firing = pd.DataFrame(columns=['position_cm', 'trial_number', 'trial_type', 'dwell_time_ms'])
-    for cluster in range(len(spike_data)):
-        cluster_index = spike_data.cluster_id.values[cluster] - 1
-        cluster_firing_indices = spike_data.firing_times[cluster_index]
-        spatial_firing = spatial_firing.append({
-            "position_cm": list(spatial_data.position_cm[cluster_firing_indices]),
-            "trial_number": list(spatial_data.trial_number[cluster_firing_indices]),
-            "trial_type":  list(spatial_data.trial_type[cluster_firing_indices]),
-            "dwell_time_ms":  list(spatial_data.dwell_time_ms[cluster_firing_indices]),
-        }, ignore_index=True)
-        spike_data['position_cm'] = spatial_firing.position_cm
-        spike_data['trial_number'] = spatial_firing.trial_number
-        spike_data['trial_type'] = spatial_firing.trial_type
-        spike_data['dwell_time_ms'] = spatial_firing.dwell_time_ms
-        print('Firing locations have been extracted for each cluster')
+    #for cluster in range(len(spike_data)):
+    cluster = 0
+    #cluster_index = spike_data.cluster_id.values[cluster] - 1
+    cluster_firing_indices = spike_data.firing_times[cluster]
+    spike_data = spike_data.append({
+        "position_cm": list(spatial_data.position_cm[cluster_firing_indices]),
+        "trial_number": list(spatial_data.trial_number[cluster_firing_indices]),
+        "trial_type":  list(spatial_data.trial_type[cluster_firing_indices])
+    }, ignore_index=True)
+    print('Firing locations have been extracted for each cluster')
     return spike_data
 
 
