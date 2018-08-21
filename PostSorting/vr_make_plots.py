e484881f43f5074b4e55c0f196c7baddea02f8c7
sarahtennant <stennan2@exseed.ed.ac.uk>
2018-08-21 11:27:59 +0100

    updated average firing plots for beaconed and non beaconed

--- PostSorting/vr_make_plots.py
+++ PostSorting/vr_make_plots.py
@@ -9,12 +9,12 @@ def plot_spikes_on_track(spatial_firing):
     print('I am plotting spike rastas...')
 
     #for cluster in range(len(spatial_firing)):
-    cluster=42
+    cluster=5
     spikes_on_track = plt.figure()
     ax = spikes_on_track.add_subplot(1, 1, 1)  # specify (nrows, ncols, axnum)
 
-    ax.plot(spatial_firing.position_cm[41], spatial_firing.trial_number[41], '|', color='black', markersize=12)
-    ax.plot(spatial_firing.nonbeaconed_location[cluster], spatial_firing.nonbeaconed_trial_number[cluster], '|', color='Blue', markersize=12)
+    ax.plot(spatial_firing.beaconed_position_cm[cluster], spatial_firing.beaconed_trial_number[cluster], '|', color='black', markersize=12)
+    ax.plot(spatial_firing.nonbeaconed_position_cm[cluster], spatial_firing.nonbeaconed_trial_number[cluster], '|', color='Red', markersize=12)
     plt.show()
 
     plt.ylabel('Spikes on trials', fontsize=14, labelpad = 10)
@@ -24,10 +24,10 @@ def plot_spikes_on_track(spatial_firing):
     ax.xaxis.set_ticks_position('bottom')
 
     plot_utility.style_track_plot(ax)
-    x_max = max(spatial_firing.trial_number[41])+0.5
+    x_max = max(spatial_firing.trial_number[cluster])+0.5
     plot_utility.style_vr_plot(ax, x_max)
 
-    plt.savefig(prm.get_local_recording_folder_path() + '/Figures/' + spatial_firing.session_id[cluster] + 'track_firing_' + str(cluster + 1) + '.png')
+    plt.savefig(prm.get_local_recording_folder_path() + '/Figures/track_firing_' + str(5) + '.png')
     plt.close()
 
 
@@ -40,7 +40,8 @@ def plot_firing_rate_maps(spike_data):
         avg_spikes_on_track = plt.figure()
 
         ax = avg_spikes_on_track.add_subplot(1, 1, 1)  # specify (nrows, ncols, axnum)
-        ax.plot(range(40), spike_data.avg_spike_per_bin[cluster_index], '-')
+        ax.plot(range(40), spike_data.avg_spike_per_bin_b[cluster_index], '-', color='Black')
+        ax.plot(range(40), spike_data.avg_spike_per_bin_nb[cluster_index], '-', color='Red')
         ax.locator_params(axis = 'x', nbins=3)
         ax.set_xticklabels(['0', '100', '200'])
         plt.ylabel('Avg spikes', fontsize=14, labelpad = 10)
