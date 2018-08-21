b1017d26d112fba703c3b48876d8c303dd7255e8
sarahtennant <stennan2@exseed.ed.ac.uk>
2018-08-21 09:35:31 +0100

    updated find_firing_location_indices to write data to correct index in dataframe

--- PostSorting/vr_spatial_firing.py
+++ PostSorting/vr_spatial_firing.py
@@ -5,24 +5,22 @@ import numpy as np
 prm = PostSorting.parameters.Parameters()
 
 
+
 def find_firing_location_indices(spike_data, spatial_data):
     print('I am extracting firing locations...')
     #for cluster in range(len(spike_data)):
     cluster = 5
     #cluster_index = spike_data.cluster_id.values[cluster] - 1
     cluster_firing_indices = spike_data.firing_times[cluster]
-    spike_data = spike_data.append({
-        "position_cm": list(spatial_data.position_cm[cluster_firing_indices]),
-        "trial_number": list(spatial_data.trial_number[cluster_firing_indices]),
-        "trial_type":  list(spatial_data.trial_type[cluster_firing_indices])
-    }, ignore_index=True)
 
+    spike_data.loc[cluster].position_cm = list(spatial_data.position_cm[cluster_firing_indices])
+    spike_data.loc[cluster].trial_number = list(spatial_data.trial_number[cluster_firing_indices])
+    spike_data.loc[cluster].trial_type = list(spatial_data.trial_type[cluster_firing_indices])
     print('Firing locations have been extracted for each cluster')
     return spike_data
 
 
 def process_spatial_firing(spike_data, spatial_data):
-
     spatial_firing = find_firing_location_indices(spike_data, spatial_data)
 
     return spatial_firing
@@ -31,7 +29,7 @@ def process_spatial_firing(spike_data, spatial_data):
 
 
 def split_spatial_firing_by_trial_type(spike_data):
-    cluster_index=41
+    cluster_index=5
     cluster_df = spike_data.iloc[[cluster_index]] # dataframe for that cluster
     trials = np.array(cluster_df.trial_number.tolist())
     locations = np.array(cluster_df.position_cm.tolist())
