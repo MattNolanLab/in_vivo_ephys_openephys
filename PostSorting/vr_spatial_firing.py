bbd97cabf2624b851ff973e29ca2846fa8d4a461
Klara Gerlei <klarizsofi@gmail.com>
2018-09-17 14:09:57 +0100

    memory error fix

--- PostSorting/vr_spatial_firing.py
+++ PostSorting/vr_spatial_firing.py
@@ -23,6 +23,7 @@ def add_columns_to_dataframe(spike_data):
 
 def add_position_x(spike_data, spatial_data_x):
     for cluster_index in range(len(spike_data) - 1):
+        cluster_index = spike_data.cluster_id.values[cluster_index] - 1
         cluster_firing_indices = spike_data.firing_times[cluster_index]
         spike_data.x_position_cm[cluster_index] = spatial_data_x[cluster_firing_indices]
     return spike_data
@@ -30,15 +31,17 @@ def add_position_x(spike_data, spatial_data_x):
 
 def add_trial_number(spike_data, spatial_data_trial_number):
     for cluster_index in range(len(spike_data) - 1):
+        cluster_index = spike_data.cluster_id.values[cluster_index] - 1
         cluster_firing_indices = spike_data.firing_times[cluster_index]
-        spike_data.trial_number[cluster_index] = spatial_data_trial_number[cluster_firing_indices]
+        spike_data.trial_number[cluster_index] = spatial_data_trial_number[cluster_firing_indices].values.astype(np.uint16)
     return spike_data
 
 
 def add_trial_type(spike_data, spatial_data_trial_type):
     for cluster_index in range(len(spike_data) - 1):
+        cluster_index = spike_data.cluster_id.values[cluster_index] - 1
         cluster_firing_indices = spike_data.firing_times[cluster_index]
-        spike_data.trial_type[cluster_index] = spatial_data_trial_type[cluster_firing_indices]
+        spike_data.trial_type[cluster_index] = spatial_data_trial_type[cluster_firing_indices].astype(np.uint8)
     return spike_data
 
 
@@ -57,12 +60,12 @@ def split_spatial_firing_by_trial_type(spike_data):
     print('I am splitting firing locations by trial type...')
     #cluster_index = 5
     for cluster_index in range(len(spike_data)-1):
-        #cluster_index = spike_data.cluster_id.values[cluster] - 1
+        cluster_index = spike_data.cluster_id.values[cluster_index] - 1
         cluster_df = spike_data.iloc[[cluster_index]] # dataframe for that cluster
         trials = np.array(cluster_df['trial_number'].tolist())
         locations = np.array(cluster_df['x_position_cm'].tolist())
         trial_type = np.array(cluster_df['trial_type'].tolist())
-
+        # index out of range error in following line
         beaconed_locations = np.take(locations, np.where(trial_type == 0)[1]) #split location and trial number
         nonbeaconed_locations = np.take(locations,np.where(trial_type == 1)[1])
         probe_locations = np.take(locations, np.where(trial_type == 2)[1])
