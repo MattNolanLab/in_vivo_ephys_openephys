f2c1f3309a148170e5e6daf9e23db99f502df4d3
Klara Gerlei <klarizsofi@gmail.com>
2018-10-03 13:23:49 +0100

    changed ylim back to

--- PostSorting/make_plots.py
+++ PostSorting/make_plots.py
@@ -117,7 +117,6 @@ def plot_autocorrelograms(spike_data, prm):
 
 def plot_spikes_for_channel(grid, highest_value, lowest_value, spike_data, cluster, channel):
     snippet_plot = plt.subplot(grid[int(channel/2), channel % 2])
-    #highest_value = np.max(np.mean(spike_data.random_snippets[cluster][channel, :, :], 1))
     plt.ylim(lowest_value - 10, highest_value + 30)
     plot_utility.style_plot(snippet_plot)
     snippet_plot.plot(spike_data.random_snippets[cluster][channel, :, :] * -1, color='lightslategray')
@@ -132,7 +131,7 @@ def plot_waveforms(spike_data, prm):
     for cluster in range(len(spike_data)):
         cluster = spike_data.cluster_id.values[cluster] - 1
         max_channel = spike_data.primary_channel[cluster]
-        highest_value = np.median(spike_data.random_snippets[cluster][max_channel-1, :, :] * -1)
+        highest_value = np.max(spike_data.random_snippets[cluster][max_channel-1, :, :] * -1)
         lowest_value = np.min(spike_data.random_snippets[cluster][max_channel-1, :, :] * -1)
         fig = plt.figure(figsize=(5, 5))
         grid = plt.GridSpec(2, 2, wspace=0.5, hspace=0.5)
