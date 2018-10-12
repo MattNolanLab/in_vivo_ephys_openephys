f28998b5930c5aef1799f1017beaee7d6c62833b
sarahtennant <stennan2@exseed.ed.ac.uk>
2018-10-12 15:57:46 +0100

    changed dtype of raw data input to dataframe

--- PostSorting/post_process_sorted_data_vr.py
+++ PostSorting/post_process_sorted_data_vr.py
@@ -33,6 +33,9 @@ def process_position_data(recording_to_process, prm):
 
 def make_plots(spike_data, spatial_data):
     PostSorting.vr_make_plots.plot_stops_on_track(spatial_data)
+    PostSorting.vr_make_plots.plot_stop_histogram(spatial_data)
+    PostSorting.vr_make_plots.plot_speed_histogram(spatial_data)
+    PostSorting.vr_make_plots.plot_combined_behaviour(spatial_data)
     PostSorting.make_plots.plot_waveforms(spike_data, prm)
     PostSorting.make_plots.plot_spike_histogram(spike_data, prm)
     PostSorting.vr_make_plots.plot_spikes_on_track(spike_data,spatial_data)
@@ -60,11 +63,7 @@ def post_process_recording(recording_to_process, session_type):
     create_folders_for_output(recording_to_process)
     initialize_parameters(recording_to_process)
     spatial_data = process_position_data(recording_to_process, prm)
-    PostSorting.vr_make_plots.plot_stops_on_track(spatial_data)
-    PostSorting.vr_make_plots.plot_stop_histogram(spatial_data)
-    PostSorting.vr_make_plots.plot_speed_histogram(spatial_data)
     PostSorting.vr_make_plots.plot_combined_behaviour(spatial_data)
-
     spike_data = PostSorting.load_firing_data.create_firing_data_frame(recording_to_process, session_type, prm)
     spike_data = PostSorting.temporal_firing.add_temporal_firing_properties_to_df(spike_data, prm)
     spike_data, bad_clusters = PostSorting.curation.curate_data(spike_data, prm)
@@ -88,7 +87,7 @@ def main():
 
     params = PostSorting.parameters.Parameters()
 
-    recording_folder = '/Users/sarahtennant/Work/Analysis/Opto_data/mcos/M1_D16_2018-06-21_13-38-50'
+    recording_folder = '/Users/sarahtennant/Work/Analysis/Opto_data/PVCre1/M1_D16_2018-10-11_13-55-54'
     print('Processing ' + str(recording_folder))
 
     post_process_recording(recording_folder, 'vr')
