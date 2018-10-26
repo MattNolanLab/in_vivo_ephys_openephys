93b2b0b06650d6dd1fc11bb86ef8328337b8b567
Klara Gerlei <klarizsofi@gmail.com>
2018-10-26 20:36:49 +0100

    added function to get field analysis data frame
    each row is a field so there are no nested arrays

--- data_frame_utility.py
+++ data_frame_utility.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import PostSorting.open_field_head_direction
 import numpy as np
-import data_frame_utility
 
 
 # source: https://stackoverflow.com/users/48956/user48956
@@ -13,34 +12,66 @@ def df_empty(columns, dtypes, index=None):
     return df
 
 
-def get_field_data_frame(spatial_firing):
-    #print(spatial_firing.head())
-    field_data = data_frame_utility.df_empty(['session_id', 'cluster_id', 'field_id', 'indices_rate_map', 'spike_times', 'position_x_spikes', 'position_y_spikes', 'hd_in_field_spikes', 'hd_hist', ])
+def append_field_to_data_frame(field_df, session_id, cluster_id, field_id, indices_rate_map, spike_times, number_of_spikes_in_field, position_x_spikes, position_y_spikes, hd_in_field_spikes, hd_hist_spikes, times_session, time_spent_in_field, position_x_session, position_y_session, hd_in_field_session, hd_hist_session):
+    field_df = field_df.append({
+        "session_id": session_id,
+        "cluster_id":  cluster_id,
+        "field_id": field_id,
+        "indices_rate_map": indices_rate_map,
+        "spike_times": spike_times,
+        "number_of_spikes_in_field": number_of_spikes_in_field,
+        "position_x_spikes": position_x_spikes,
+        "position_y_spikes": position_y_spikes,
+        "hd_in_field_spikes": hd_in_field_spikes,
+        "hd_hist_spikes": hd_hist_spikes,
+        "times_session": times_session,
+        "time_spent_in_field": time_spent_in_field,
+        "position_x_session": position_x_session,
+        "position_y_session": position_y_session,
+        "hd_in_field_session": hd_in_field_session,
+        "hd_hist_session": hd_hist_session
+    }, ignore_index=True)
+    return field_df
+
+
+def get_field_data_frame(spatial_firing, position_data):
+    field_df = pd.DataFrame(columns=['session_id', 'cluster_id', 'field_id', 'indices_rate_map', 'spike_times', 'number_of_spikes_in_field', 'position_x_spikes', 'position_y_spikes', 'hd_in_field_spikes', 'hd_hist_spikes', 'times_session', 'time_spent_in_field', 'position_x_session', 'position_y_session', 'hd_in_field_session', 'hd_hist_session'])
     for cluster in range(len(spatial_firing)):
-        number_of_spikes_in_fields = []
-        number_of_samples_in_fields = []
-        hd_in_fields_cluster = []
-        hd_in_field_sessions = []
         cluster = spatial_firing.cluster_id.values[cluster] - 1
+        cluster_id = spatial_firing.cluster_id[cluster]
+        session_id = spatial_firing.session_id[cluster]
         number_of_firing_fields = len(spatial_firing.firing_fields[cluster])
         if number_of_firing_fields > 0:
             firing_field_spike_times = spatial_firing.spike_times_in_fields[cluster]
-            firing_field_times_session = spatial_firing.times_in_session_fields[cluster]
             for field_id, field in enumerate(firing_field_spike_times):
+                indices_rate_map = spatial_firing.firing_fields[cluster][field_id]
                 mask_firing_times_in_field = np.in1d(spatial_firing.firing_times[cluster], field)
-                firing_field_times_session = spatial_firing.times_in_session_fields[cluster][field_id]
-                number_of_spikes_field = len(field)
-                hd_field_cluster = np.array(spatial_firing.hd[cluster])[mask_firing_times_in_field]
-                hd_field_cluster = (np.array(hd_field_cluster) + 180) * np.pi / 180
-                hd_fields_cluster_hist = PostSorting.open_field_head_direction.get_hd_histogram(hd_field_cluster)
-                number_of_spikes_in_fields.append(number_of_spikes_field)
-                hd_in_fields_cluster.append(hd_fields_cluster_hist)
+                spike_times = field
+                number_of_spikes_in_field = len(field)
+                position_x_spikes = np.array(spatial_firing.position_x_pixels[cluster])[mask_firing_times_in_field]
+                position_y_spikes = np.array(spatial_firing.position_y_pixels[cluster])[mask_firing_times_in_field]
+                hd_in_field_spikes = np.array(spatial_firing.hd[cluster])[mask_firing_times_in_field]
+                hd_in_field_spikes = (np.array(hd_in_field_spikes) + 180) * np.pi / 180
+                hd_hist_spikes = PostSorting.open_field_head_direction.get_hd_histogram(hd_in_field_spikes)
+
+                times_session = spatial_firing.times_in_session_fields[cluster][field_id]
+                time_spent_in_field = len(times_session)
+                mask_times_in_field = np.in1d(position_data.synced_time, times_session)
+                position_x_session = position_data.position_x_pixels.values[mask_times_in_field]
+                position_y_session = position_data.position_y_pixels.values[mask_times_in_field]
+                hd_in_field_session = position_data.hd.values[mask_times_in_field]
+                hd_in_field_session = (np.array(hd_in_field_session) + 180) * np.pi / 180
+                hd_hist_session = PostSorting.open_field_head_direction.get_hd_histogram(hd_in_field_session)
+
+                field_df = append_field_to_data_frame(field_df, session_id, cluster_id, field_id, indices_rate_map, spike_times, number_of_spikes_in_field, position_x_spikes, position_y_spikes, hd_in_field_spikes, hd_hist_spikes, times_session, time_spent_in_field, position_x_session, position_y_session, hd_in_field_session, hd_hist_session)
+    return field_df
 
 
 
 def main():
     spatial_firing = pd.read_pickle('C:/Users/s1466507\Documents/Ephys/test_overall_analysis/M5_2018-03-06_15-34-44_of/DataFrames/spatial_firing.pkl')
-    get_field_data_frame(spatial_firing)
+    position_data = pd.read_pickle('C:/Users/s1466507\Documents/Ephys/test_overall_analysis/M5_2018-03-06_15-34-44_of/DataFrames/position.pkl')
+    get_field_data_frame(spatial_firing, position_data)
 
 if __name__ == '__main__':
     main()
