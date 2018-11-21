1abba3ab584a21f7aed3e900574570c2841e3559
sarahtennant <stennan2@exseed.ed.ac.uk>
2018-11-21 18:41:47 +0000

    split split_dataframe_columns_by_speed into 3 seperate functions and corrected indexing bug

--- PostSorting/vr_spatial_firing.py
+++ PostSorting/vr_spatial_firing.py
@@ -68,24 +68,34 @@ def find_firing_location_indices(spike_data, spatial_data):
     return spike_data
 
 
-def split_dataframe_columns_by_speed(cluster_index,spike_data,spike_data_movement,spike_data_stationary,above_threshold_indices,below_threshold_indices):
-    spike_data_movement.trial_number[cluster_index] = spike_data.trial_number[above_threshold_indices].values
-    spike_data_movement.x_position_cm[cluster_index] = spike_data.x_position_cm[above_threshold_indices].values
-    spike_data_movement.trial_type[cluster_index] = spike_data.trial_type[above_threshold_indices].values
+def split_and_add_trial_number(cluster_index, spike_data_movement, spike_data_stationary, spike_data_trial_number,above_threshold_indices,below_threshold_indices):
+    spike_data_movement.trial_number[cluster_index] = spike_data_trial_number[above_threshold_indices]
+    spike_data_stationary.trial_number[cluster_index] = spike_data_trial_number[below_threshold_indices]
+    return spike_data_movement, spike_data_stationary
 
-    spike_data_stationary.trial_number[cluster_index] = spike_data.trial_number[below_threshold_indices].values
-    spike_data_stationary.x_position_cm[cluster_index] = spike_data.x_position_cm[below_threshold_indices].values
-    spike_data_stationary.trial_type[cluster_index] = spike_data.trial_type[below_threshold_indices].values
-    return spike_data_movement,spike_data_stationary
+
+def split_and_add_x_location_cm(cluster_index, spike_data_movement, spike_data_stationary, spike_data_x_location_cm,above_threshold_indices,below_threshold_indices):
+    spike_data_movement.x_position_cm[cluster_index] = spike_data_x_location_cm[above_threshold_indices]
+    spike_data_stationary.x_position_cm[cluster_index] = spike_data_x_location_cm[below_threshold_indices]
+    return spike_data_movement, spike_data_stationary
+
+
+def split_and_add_trial_type(cluster_index, spike_data_movement, spike_data_stationary, spike_data_trial_type,above_threshold_indices,below_threshold_indices):
+    spike_data_movement.trial_type[cluster_index] = spike_data_trial_type[above_threshold_indices]
+    spike_data_stationary.trial_type[cluster_index] = spike_data_trial_type[below_threshold_indices]
+    return spike_data_movement, spike_data_stationary
 
 
 def split_spatial_firing_by_speed(spike_data, spike_data_movement, spike_data_stationary):
     movement_threshold=5 # 5 cm / second
     for cluster_index in range(len(spike_data)):
         cluster_index = spike_data.cluster_id.values[cluster_index] - 1
-        above_threshold_indices = np.where(spike_data.speed_per200ms[cluster_index] > movement_threshold)[0]
-        below_threshold_indices = np.where(spike_data.speed_per200ms[cluster_index] < movement_threshold)[0]
-        spike_data_movement, spike_data_stationary = split_dataframe_columns_by_speed(cluster_index,spike_data,spike_data_movement,spike_data_stationary,above_threshold_indices,below_threshold_indices)
+        above_threshold_indices = np.where(np.array(spike_data.speed_per200ms[cluster_index]) > movement_threshold)[0]
+        below_threshold_indices = np.where(np.array(spike_data.speed_per200ms[cluster_index]) < movement_threshold)[0]
+
+        spike_data_movement, spike_data_stationary = split_and_add_trial_number(cluster_index,spike_data_movement, spike_data_stationary, np.array(spike_data.trial_number[cluster_index]),above_threshold_indices,below_threshold_indices)
+        spike_data_movement, spike_data_stationary = split_and_add_x_location_cm(cluster_index,spike_data_movement, spike_data_stationary, np.array(spike_data.x_position_cm[cluster_index]),above_threshold_indices,below_threshold_indices)
+        spike_data_movement, spike_data_stationary = split_and_add_trial_type(cluster_index,spike_data_movement, spike_data_stationary, np.array(spike_data.trial_type[cluster_index]),above_threshold_indices,below_threshold_indices)
     return spike_data, spike_data_movement, spike_data_stationary
 
 
