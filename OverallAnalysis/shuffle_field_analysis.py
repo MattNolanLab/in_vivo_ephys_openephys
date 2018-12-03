d94944df385d788cab3492494e09a415fabb928c
Klara Gerlei <klarizsofi@gmail.com>
2018-12-03 13:51:14 +0000

    shuffle data for all sessions on server

--- OverallAnalysis/shuffle_field_analysis.py
+++ OverallAnalysis/shuffle_field_analysis.py
@@ -1,3 +1,4 @@
+import glob
 import os
 import numpy as np
 import pandas as pd
@@ -5,6 +6,10 @@ import OverallAnalysis.false_positives
 import data_frame_utility
 
 import matplotlib.pylab as plt
+
+
+server_path = '//cmvm.datastore.ed.ac.uk/cmvm/sbms/groups/mnolan_NolanLab/ActiveProjects/Klara/Open_field_opto_tagging_p038/'
+server_test_file = '//cmvm.datastore.ed.ac.uk/cmvm/sbms/groups/mnolan_NolanLab/ActiveProjects/Klara/test_analysis/M5_2018-03-05_13-30-30_of/parameters.txt'
 
 
 def add_combined_id_to_df(df_all_mice):
@@ -37,44 +42,58 @@ def get_field_data():
     # use tmp data for now from a selected cell / field
 
 
-def shuffle_field_data(field_data, number_of_times_to_shuffle, local_path):
-    if os.path.exists(local_path + 'shuffle_analysis') is False:
-        os.makedirs(local_path + 'shuffle_analysis')
+def shuffle_field_data(field_data, number_of_times_to_shuffle, path):
+    if os.path.exists(path + 'shuffle_analysis') is False:
+        os.makedirs(path + 'shuffle_analysis')
 
     for index, field in field_data.iterrows():
-        print(field)
+        print('I will shuffle data in the fields.')
         number_of_spikes_in_field = field['number_of_spikes_in_field']
         time_spent_in_field = field['time_spent_in_field']
         shuffle_indices = np.random.randint(0, time_spent_in_field, size=(number_of_times_to_shuffle, number_of_spikes_in_field))
         plt.cla()
         plt.hist(field['hd_in_field_spikes'], color='red')
-        plt.savefig(local_path + 'shuffle_analysis/hd_cluster_' + str(field['cluster_id']) + '_field_' + str(index))
+        plt.savefig(path + 'shuffle_analysis/hd_cluster_' + str(field['cluster_id']) + '_field_' + str(index))
         plt.close()
 
         for shuffle in range(number_of_times_to_shuffle):
             shuffled_hd = field['hd_in_field_session'][shuffle_indices[shuffle]]
             plt.hist(shuffled_hd, color='navy')
-            plt.savefig(local_path + 'shuffle_analysis/hd_cluster_' + str(field['cluster_id']) + '_field_' + str(index) + '_shuffle' + str(shuffle))
+            plt.savefig(path + 'shuffle_analysis/hd_cluster_' + str(field['cluster_id']) + '_field_' + str(index) + '_shuffle' + str(shuffle))
             plt.close()
+    print(path)
+    return
 
-    return field_data
+
+def process_recordings():
+    if os.path.exists(server_test_file):
+        print('I see the server.')
+    for recording_folder in glob.glob(server_path + '*'):
+        os.path.isdir(recording_folder)
+        spike_data_frame_path = recording_folder + '/MountainSort/DataFrames/spatial_firing.pkl'
+        position_data_frame_path = recording_folder + '/MountainSort/DataFrames/position.pkl'
+        if os.path.exists(spike_data_frame_path):
+            print('I found a firing data frame.')
+            spatial_firing = pd.read_pickle(spike_data_frame_path)
+            position_data = pd.read_pickle(position_data_frame_path)
+            field_df = data_frame_utility.get_field_data_frame(spatial_firing, position_data)
+            shuffle_field_data(field_df, 20, recording_folder + '/MountainSort/')
+
+
+def local_data_test():
+    local_path = '/Users/s1466507/Documents/Ephys/recordings/M12_2018-04-10_14-22-14_of/MountainSort/'
+
+    spatial_firing = pd.read_pickle(local_path + '/DataFrames/spatial_firing.pkl')
+    position_data = pd.read_pickle(local_path + '/DataFrames/position.pkl')
+
+    field_df = data_frame_utility.get_field_data_frame(spatial_firing, position_data)
+    shuffle_field_data(field_df, 20, local_path)
 
 
 def main():
     print('-------------------------------------------------------------')
     print('-------------------------------------------------------------')
-    # get_field_data()
-    local_path = '/Users/s1466507/Documents/Ephys/recordings/M5_2018-03-06_15-34-44_of/MountainSort/'
-    spatial_firing = pd.read_pickle(local_path + '/DataFrames/spatial_firing.pkl')
-    position_data = pd.read_pickle(local_path + '/DataFrames/position.pkl')
-
-    field_df = data_frame_utility.get_field_data_frame(spatial_firing, position_data)
-    shuffled_data = shuffle_field_data(field_df, 20, local_path)
-
-    #field_spike_times = spike_data.spike_times_in_fields[5][0]
-    #times_in_session_fields = spike_data.times_in_session_fields[5][0]
-    pass
-
+    process_recordings()
 
 
 
