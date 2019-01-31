2778898071d4b09cf0c7c0cae8dbc49a05624b6b
Klara Gerlei <klarizsofi@gmail.com>
2019-01-31 11:10:39 +0000

    load data from matlab file
    example line added - needs to load all data and put it in a python data frame so the analysis code can run on it

---
+++ OverallAnalysis/grid_analysis_other_labs/load_grid_data_from_matlab.py
@@ -0,0 +1,6 @@
+from mat4py import loadmat
+
+data = loadmat('//ardbeg.mvm.ed.ac.uk/nolanlab/Klara/grid_field_analysis/moser_data/Sargolini/11084-03020501_t2c1.mat')
+print('worked')
+
+# next: load all data into data frames that are the same format as in the analysis pipeline
