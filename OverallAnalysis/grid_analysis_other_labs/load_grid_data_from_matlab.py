25673aa4fca946130f067028a7ba3b4f57521478
Klara Gerlei <klarizsofi@gmail.com>
2019-01-31 16:22:18 +0000

    load all data + notes on what is needed in next steps

--- OverallAnalysis/grid_analysis_other_labs/load_grid_data_from_matlab.py
+++ OverallAnalysis/grid_analysis_other_labs/load_grid_data_from_matlab.py
@@ -1,6 +1,8 @@
+import glob
 from mat4py import loadmat
 import matplotlib.pylab as plt
 import numpy as np
+import os
 import pandas as pd
 import PostSorting.open_field_spatial_data
 
@@ -22,12 +24,25 @@ def get_position_data_frame(matlab_data):
     position_data = PostSorting.open_field_spatial_data.calculate_head_direction(position_data)  # use coord from the two beads to get hd and interpolate
     position_data = PostSorting.open_field_spatial_data.shift_to_start_from_zero_at_bottom_left(position_data)
     position_data = PostSorting.open_field_spatial_data.calculate_central_speed(position_data)
-    position_of_mouse = position_data[['time_seconds', 'position_x', 'position_x_pixels', 'position_y', 'position_y_pixels', 'hd', 'speed']].copy()
+    position_of_mouse = position_data[['time_seconds', 'position_x', 'position_y', 'hd', 'speed']].copy()
     # plt.plot(position_data.position_x, position_data.position_y) # this is to plot the trajectory. it looks weird
     return position_of_mouse
 
 
+def process_data(folder_to_search_in):
+    for name in glob.glob(folder_to_search_in + '/*.mat'):
+        if os.path.exists(name):
+            print('I found this cell:' + name)
+            cell = loadmat(name)
+            position_data = get_position_data_frame(cell)
+            print('cat')
+            print(position_data.head())
+            # figure out session id here and load firing data from all cells for each session so each trajectory is only processed once
+
+
+
 def main():
+    process_data('//ardbeg.mvm.ed.ac.uk/nolanlab/Klara/grid_field_analysis/moser_data/Sargolini')
     matlab_data = loadmat('//ardbeg.mvm.ed.ac.uk/nolanlab/Klara/grid_field_analysis/moser_data/Sargolini/11084-03020501_t2c1.mat')
     # todo: iterate over all data and load them for analysis - get session ID so cells can be added to session. iterate in all_data
     position_data = get_position_data_frame(matlab_data)
