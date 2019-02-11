e6aab3871edce32f37ea8ecc65bf354845492eb5
Klara Gerlei <klarizsofi@gmail.com>
2019-02-11 21:59:25 +0000

    call R script to count modes
    this works, but it is very slow. possibly because of the environment
    the code seems ok

---
+++ OverallAnalysis/count_modes_circular.R
@@ -0,0 +1,49 @@
+
+
+count_modes <- function(hd){
+    #install.packages("useful")
+    #install.packages("movMF")
+    X <- read.csv("C:/Users/s1466507/Documents/GitHub/in_vivo_ephys_openephys/OverallAnalysis/M13_2018_05_09_c17.csv")
+    print(file.path(R.home("bin"), "R"))
+    print(R.version)
+
+    # and brought them to radians using
+    print(str(X))
+    print(typeof(X[1]))
+    X <- data.frame(lapply(hd, as.double))
+    print(str(X))
+    print(typeof(X[1]))
+
+    #print(str(Y))
+    #print(typeof(Y[1]))
+    #X <- data.frame(lapply(X, as.numeric))
+    # and brought them to radians using
+    X[,1] <- X[,1]*pi/180
+    X     <- cbind(X, rep(1, nrow(X)))
+    # next I converted to cartesian co-ordinates in order to pass the data of right type to function movMF
+    library(useful)
+    cartcoords <- (pol2cart(r=X[,2], theta=X[,1]))[,1:2]
+    # Next, I fitted mixture models with 1,2,., 30 models and computed the
+    # AIC in order to decide what model fits the data best from the given
+    # class of models. The code for the fitting part is
+    ## fitting mvM
+    library(movMF)
+    M <- 12
+    fit <- vector('list', M)
+
+
+    for(i in 1:M){
+      fit[[i]] <- movMF(as.matrix(cartcoords), k=i, control=list(maxiter=1000))
+        print(i)
+        flush.console()
+        }
+
+
+    pars <- 2*seq(1:M) + 0:(M-1)
+    cov  <- 1:M
+    aic  <- -2*(loess(unlist(lapply(fit, function(x) "[["(x, "L")))~cov)$fitted - pars)
+
+    library(ggplot2)
+    plot(aic, type="l", ylab="AIC", xlab="Number of components (or modes??)")
+    return(aic)
+    }
--- OverallAnalysis/count_modes_of_hd_distributions.py
+++ OverallAnalysis/count_modes_of_hd_distributions.py
@@ -14,22 +14,26 @@ def count_modes_of_hd_distributions():
     for index, cluster in spatial_firing.iterrows():
         # todo need to convert this to an R object http://rpy.sourceforge.net/rpy2/doc-2.2/html/vector.html
         # hd_cluster_r = r.vectors.FloatVector(hd_cluster)
-        # hd_cluster = pd.DataFrame(cluster.hd)
-        # hd_cluster_r = pandas2ri.py2ri(hd_cluster)
-        if index < 10:
+        cluster_hd_np = np.array(cluster.hd)
+        cluster_hd_np = cluster_hd_np[~np.isnan(cluster_hd_np)]
+        cluster_hd_np = np.round(cluster_hd_np, 4)
+        hd_cluster = pd.DataFrame({'hd':cluster_hd_np})
+        hd_cluster_r = pandas2ri.py2ri(hd_cluster)
+        if index < 20:
             continue
-        hd_cluster = list(cluster.hd)
+        # hd_cluster = list(cluster.hd)
         # np.savetxt('hd_cluster.csv', hd_cluster)
-        hd_cluster = robj.DataFrame({'a':cluster.hd})
+        # hd_cluster = robj.DataFrame({'label':range(len(hd_cluster)),'hd':cluster.hd})
 
         # path = os.path.dirname(os.path.realpath(__file__)) + 'hd_cluster_csv'
         # path_r = r.vectors.StrVector(path)
         # r_vector = robj.FloatVector(hd_cluster)
-        robj.r.source('count_modes_of_circular_dist.R')
+        robj.r.source('count_modes_circular.R')
+        #print(hd_cluster)
         # aic = r.r['count_number_of_modes'](r_vector)  # turn this back to python object
-        rcall = robj.r['count_number_of_modes']
-        test=rcall(hd_cluster)
-        print("ede")
+        rcall = robj.r['count_modes']
+        test = rcall(hd_cluster_r)
+        print(test)
 
     # rerun load df function to get hd from all recordings...
 
