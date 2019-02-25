787ca25fcbaa75decb12f32b98e30fe70c41f029
Klara Gerlei <klarizsofi@gmail.com>
2019-02-25 16:01:54 +0000

    plot hd vs stat

--- OverallAnalysis/analyze_hd_from_whole_session.py
+++ OverallAnalysis/analyze_hd_from_whole_session.py
@@ -11,10 +11,19 @@ def load_data_frame(path):
     return df
 
 
-def plot_hd_vs_watson_stat(df_all_mice):
-    plt.figure()
-    hd_score = df_all_mice.hd_score
-    watson_two_stat = df_all_mice.watson_test_hd
+def plot_hd_vs_watson_stat(df_all_mice, save_output_path):
+    good_cluster = df_all_mice.false_positive == False
+    fig, ax = plt.subplots()
+    hd_score = df_all_mice[good_cluster].hd_score
+    watson_two_stat = df_all_mice[good_cluster].watson_test_hd
+    plt.scatter(hd_score, watson_two_stat)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+    ax.set_xlabel('Head-direction score')
+    ax.set_ylabel('Two-sample Watson test stat')
+    plt.savefig(save_output_path + 'hd_vs_watson_stat_all_cells.png')
 
 
 def add_combined_id_to_df(df_all_mice):
@@ -25,10 +34,37 @@ def add_combined_id_to_df(df_all_mice):
 
     combined_ids = []
     for cell in range(len(df_all_mice)):
-        id = animal_ids[cell] +  '-' + dates[cell] + '-Tetrode-' + str(tetrode[cell]) + '-Cluster-' + str(cluster[cell])
+        id = animal_ids[cell] + '-' + dates[cell] + '-Tetrode-' + str(tetrode[cell]) + '-Cluster-' + str(cluster[cell])
         combined_ids.append(id)
     df_all_mice['false_positive_id'] = combined_ids
     return df_all_mice
+
+
+def correlation_between_first_and_second_halves_of_session(df_all_mice, save_output_path):
+    excitatory_neurons = df_all_mice.mean_firing_rate <= 10
+    inhibitory_neurons = df_all_mice.mean_firing_rate > 10
+    good_cluster = df_all_mice.false_positive == False
+    significant_corr = df_all_mice.hd_correlation_first_vs_second_half_p < 0.001
+    watson_result_exists = df_all_mice.watson_test_hd.notnull()
+    is_hd_cell = df_all_mice.hd_score >= 0.5
+    print('Number of cells included in two sample watson test for head-direction from the whole session: ' + str(
+        watson_result_exists.sum()))
+    print('excitatory: ' + str(len(df_all_mice[excitatory_neurons & watson_result_exists])))
+    print('inhibitory: ' + str(len(df_all_mice[inhibitory_neurons & watson_result_exists])))
+    watson_significant = df_all_mice.watson_test_hd > 0.385  # p < 0.001
+    print('Number of cells with significantly different HD distributions: ' + str(watson_significant.sum()))
+    print('Number of excitatory neurons with significantly different HD: ' + str(len(df_all_mice[watson_significant & excitatory_neurons])))
+    print('Number of inhibitory neurons with significantly different HD: ' + str(len(df_all_mice[watson_significant & inhibitory_neurons])))
+
+    print('Number of excitatory neurons with significantly different HD that are hd cells: ' + str(len(df_all_mice[watson_significant & excitatory_neurons & is_hd_cell])))
+    print('Number of inhibitory neurons with significantly different HD that are hd cells: ' + str(len(df_all_mice[watson_significant & inhibitory_neurons & is_hd_cell])))
+
+    print('mean pearson r of correlation between first and second half')
+    print(df_all_mice.hd_correlation_first_vs_second_half[significant_corr & good_cluster].mean())
+
+    OverallAnalysis.analyze_field_correlations.plot_correlation_coef_hist(df_all_mice.hd_correlation_first_vs_second_half[significant_corr & good_cluster & watson_significant], save_output_path + 'correlation_hd_session.png')
+    OverallAnalysis.analyze_field_correlations.plot_correlation_coef_hist(df_all_mice.hd_correlation_first_vs_second_half[significant_corr & good_cluster & watson_significant & excitatory_neurons], save_output_path + 'correlation_hd_session_excitatory.png')
+    OverallAnalysis.analyze_field_correlations.plot_correlation_coef_hist(df_all_mice.hd_correlation_first_vs_second_half[significant_corr & good_cluster & watson_significant & inhibitory_neurons], save_output_path + 'correlation_hd_session_inhibitory.png')
 
 
 def main():
@@ -42,35 +78,8 @@ def main():
     list_of_false_positives = OverallAnalysis.false_positives.get_list_of_false_positives(false_positives_path)
     df_all_mice = add_combined_id_to_df(df_all_mice)
     df_all_mice['false_positive'] = df_all_mice['false_positive_id'].isin(list_of_false_positives)
-
-    good_cluster = df_all_mice.false_positive == False
-
-    excitatory_neurons = df_all_mice.mean_firing_rate <= 10
-    inhibitory_neurons = df_all_mice.mean_firing_rate > 10
-    significant_corr = df_all_mice.hd_correlation_first_vs_second_half_p < 0.001
-    print('mean pearson r of correlation between first and second half')
-    print(df_all_mice.hd_correlation_first_vs_second_half[significant_corr & good_cluster].mean())
-
-    watson_result_exists = df_all_mice.watson_test_hd.notnull()
-    is_hd_cell = df_all_mice.hd_score >= 0.5
-    print('Number of cells included in two sample watson test for head-direction from the whole session: ' + str(watson_result_exists.sum()))
-    print('excitatory: ' + str(len(df_all_mice[excitatory_neurons & watson_result_exists])))
-    print('inhibitory: ' + str(len(df_all_mice[inhibitory_neurons & watson_result_exists])))
-    watson_significant = df_all_mice.watson_test_hd > 0.385  # p < 0.001
-    print('Number of cells with significantly differen HD distributions: ' + str(watson_significant.sum()))
-    print('Number of excitatory neurons with significantly different HD: ' + str(len(df_all_mice[watson_significant & excitatory_neurons])))
-    print('Number of inhibitory neurons with significantly different HD: ' + str(len(df_all_mice[watson_significant & inhibitory_neurons])))
-
-    print('Number of excitatory neurons with significantly different HD that are hd cells: ' + str(len(df_all_mice[watson_significant & excitatory_neurons & is_hd_cell])))
-    print('Number of inhibitory neurons with significantly different HD that are hd cells: ' + str(len(df_all_mice[watson_significant & inhibitory_neurons & is_hd_cell])))
-
+    correlation_between_first_and_second_halves_of_session(df_all_mice, save_output_path)
     plot_hd_vs_watson_stat(df_all_mice)
-    # find those where the hd polar plot significantly differs in the first vs second half
-    # run load_df again and add the result of the 2 sample watson test to the big df and then filter for that here
-    OverallAnalysis.analyze_field_correlations.plot_correlation_coef_hist(df_all_mice.hd_correlation_first_vs_second_half[significant_corr & good_cluster & watson_significant], save_output_path + 'correlation_hd_session.png')
-    OverallAnalysis.analyze_field_correlations.plot_correlation_coef_hist(df_all_mice.hd_correlation_first_vs_second_half[significant_corr & good_cluster & watson_significant & excitatory_neurons], save_output_path + 'correlation_hd_session_excitatory.png')
-    OverallAnalysis.analyze_field_correlations.plot_correlation_coef_hist(df_all_mice.hd_correlation_first_vs_second_half[significant_corr & good_cluster & watson_significant & inhibitory_neurons], save_output_path + 'correlation_hd_session_inhibitory.png')
-
 
 
 if __name__ == '__main__':
