c6b5b25e3cb53628b4a9056e28dd5a55003c2478
Klara Gerlei <klarizsofi@gmail.com>
2019-02-25 17:02:22 +0000

    added fring maps

--- OverallAnalysis/load_data_frames.py
+++ OverallAnalysis/load_data_frames.py
@@ -36,7 +36,7 @@ def load_data_frame_spatial_firing(output_path):
 
             '''
             if ('hd_hist_first_half' in spatial_firing) and ('watson_test_hd' in spatial_firing):
-                spatial_firing = spatial_firing[['session_id', 'cluster_id', 'tetrode', 'number_of_spikes', 'mean_firing_rate', 'isolation', 'noise_overlap', 'peak_snr', 'hd_correlation_first_vs_second_half', 'hd_correlation_first_vs_second_half_p', 'hd_hist_first_half', 'firing_fields_hd_session', 'hd_hist_second_half', 'watson_test_hd', 'hd_score', 'hd', 'kuiper_cluster', 'watson_cluster']].copy()
+                spatial_firing = spatial_firing[['session_id', 'cluster_id', 'tetrode', 'number_of_spikes', 'mean_firing_rate', 'isolation', 'noise_overlap', 'peak_snr', 'hd_correlation_first_vs_second_half', 'hd_correlation_first_vs_second_half_p', 'hd_hist_first_half', 'firing_fields_hd_session', 'hd_hist_second_half', 'watson_test_hd', 'hd_score', 'hd', 'kuiper_cluster', 'watson_cluster', 'firing_maps']].copy()
 
                 # print(spatial_firing.head())
                 spatial_firing_data = spatial_firing_data.append(spatial_firing)
