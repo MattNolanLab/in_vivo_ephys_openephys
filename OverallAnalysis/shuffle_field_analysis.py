69f107f8389e9da43ffaec1ca53edd49167c857e
Klara Gerlei <klarizsofi@gmail.com>
2019-02-28 20:28:01 +0000

    format

--- OverallAnalysis/shuffle_field_analysis.py
+++ OverallAnalysis/shuffle_field_analysis.py
@@ -48,14 +48,16 @@ def get_field_data():
 
 
 def format_bar_chart(ax):
+    plt.gcf().subplots_adjust(bottom=0.2)
+    plt.gcf().subplots_adjust(left=0.2)
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.xaxis.set_ticks_position('bottom')
     ax.yaxis.set_ticks_position('left')
-    ax.set_xlabel('Head direction', fontsize=18)
-    ax.set_ylabel('Frequency', fontsize=18)
-    ax.xaxis.set_tick_params(labelsize=18)
-    ax.yaxis.set_tick_params(labelsize=18)
+    ax.set_xlabel('Head direction [deg]', fontsize=30)
+    ax.set_ylabel('Frequency [Hz]', fontsize=30)
+    ax.xaxis.set_tick_params(labelsize=20)
+    ax.yaxis.set_tick_params(labelsize=20)
     return ax
 
 
