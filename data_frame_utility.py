4e891b575519015c80686f4844b2bf29c064f8ed
Klara Gerlei <klarizsofi@gmail.com>
2019-03-19 15:10:39 +0000

    added grid score and hd score and spacing to df

--- data_frame_utility.py
+++ data_frame_utility.py
@@ -35,7 +35,7 @@ def append_field_to_data_frame(field_df, session_id, cluster_id, field_id, indic
 
 
 def get_field_data_frame(spatial_firing, position_data):
-    field_df = pd.DataFrame(columns=['session_id', 'cluster_id', 'field_id', 'indices_rate_map', 'spike_times', 'number_of_spikes_in_field', 'position_x_spikes', 'position_y_spikes', 'hd_in_field_spikes', 'hd_hist_spikes', 'times_session', 'time_spent_in_field', 'position_x_session', 'position_y_session', 'hd_in_field_session', 'hd_hist_session'])
+    field_df = pd.DataFrame(columns=['session_id', 'cluster_id', 'field_id', 'indices_rate_map', 'spike_times', 'number_of_spikes_in_field', 'position_x_spikes', 'position_y_spikes', 'hd_in_field_spikes', 'hd_hist_spikes', 'times_session', 'time_spent_in_field', 'position_x_session', 'position_y_session', 'hd_in_field_session', 'hd_hist_session', 'hd_score', 'grid_score', 'grid_spacing'])
     for cluster in range(len(spatial_firing)):
         cluster = spatial_firing.cluster_id.values[cluster] - 1
         cluster_id = spatial_firing.cluster_id[cluster]
