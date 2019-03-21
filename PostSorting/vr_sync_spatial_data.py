45e50264aba82f8c015fceaaf19522d7d7b4e7a8
Klara Gerlei <klarizsofi@gmail.com>
2019-03-21 10:38:41 +0000

    trunicated file sizes if too big and resulting in corrupted file

--- PostSorting/vr_sync_spatial_data.py
+++ PostSorting/vr_sync_spatial_data.py
@@ -26,6 +26,8 @@ def get_raw_location(recording_folder, prm):
     file_path = recording_folder + '/' + prm.get_movement_channel()
     if os.path.exists(file_path):
         location = open_ephys_IO.get_data_continuous(prm, file_path)
+        if location.shape[0] > 99999999:
+            location = location[:99999999]
     else:
         print('Movement data was not found.')
     location=correct_for_restart(location)
@@ -151,6 +153,8 @@ def load_first_trial_channel(recording_folder, prm):
     first = []
     file_path = recording_folder + '/' + prm.get_first_trial_channel() #todo this should bw in params, it is 100 for me, 105 for Tizzy (I don't have _0)
     trial_first = open_ephys_IO.get_data_continuous(prm, file_path)
+    if trial_first.shape[0] > 99999999:
+        trial_first = trial_first[:99999999]
     first.append(trial_first)
     return np.asarray(first, dtype=np.uint8)
 
@@ -160,6 +164,8 @@ def load_second_trial_channel(recording_folder, prm):
     second = []
     file_path = recording_folder + '/' + prm.get_second_trial_channel() #todo this should bw in params, it is 100 for me, 105 for Tizzy (I don't have _0)
     trial_second = open_ephys_IO.get_data_continuous(prm, file_path)
+    if trial_second.shape[0] > 99999999:
+        trial_second = trial_second[:99999999]
     second.append(trial_second)
     return np.asarray(second, dtype=np.uint8)
 
