50612335ca9da2244134cfae1dadcc7e77ffb142
Klara Gerlei <klarizsofi@gmail.com>
2019-04-08 12:04:57 +0100

    count modes in R

--- OverallAnalysis/count_modes_in_fields.py
+++ OverallAnalysis/count_modes_in_fields.py
@@ -1,1 +1,8 @@
+import numpy as np
+
+
 # count modes in firing fields
+
+number_of_times_to_sample = 1000
+head_direction_histogram = [0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 2, 3, 4, 5, 6, 7, 6, 5, 2, 1]  # bimodal
+resampled_distribution = rejection.sampling(number_of_times_to_sample, head_direction_histogram)
