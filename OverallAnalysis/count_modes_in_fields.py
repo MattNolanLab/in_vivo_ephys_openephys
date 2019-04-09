0f1fffe9ad843d5ff48b4b60bf38678d0f0ce85e
Klara Gerlei <klarizsofi@gmail.com>
2019-04-09 20:19:57 +0100

    save results in df - mode angles and density

--- OverallAnalysis/count_modes_in_fields.py
+++ OverallAnalysis/count_modes_in_fields.py
@@ -1,4 +1,5 @@
 import matplotlib.pylab as plt
+import math_utility
 import numpy as np
 import pandas as pd
 from rpy2 import robjects as robj
@@ -24,27 +25,60 @@ def fit_von_mises_mixed_model(resampled_distribution):
     return fit
 
 
+def get_model_fit_alpha_value(fit):
+    get_model_fit_alpha = robj.r('get_model_fit_alpha')
+    alpha = get_model_fit_alpha(fit)
+    return alpha
+
+
+def get_model_fit_theta_value(fit):
+    get_model_fit_theta = robj.r('get_model_fit_theta')
+    theta = get_model_fit_theta(fit)
+    return theta
+
+
+def get_estimated_density_function(fit):
+    get_estimated_density = robj.r('get_estimated_density')
+    estimated_density = get_estimated_density(fit)
+    return estimated_density
+
+
+def plot_modes_in_r(fit):
+    plot_modes = robj.r("plot_modes")
+    plot_modes(fit)
+
+
+def get_peaks_in_degrees(theta):
+    print(theta)
+    angles = []
+    theta_x = theta[:int(len(theta) / 2)]
+    theta_y = theta[int(len(theta) / 2):]
+    return theta_x * 180 / np.pi
+
+
 def analyze_histograms(field_data):
     robj.r.source('count_modes_circular_histogram.R')
+    mode_angles = []
+    fitted_densities = []
     for index, field in field_data.iterrows():
         hd_histogram_field = field.normalized_hd_hist
         # print(hd_histogram_field)
         if np.isnan(hd_histogram_field).sum() > 0:
             print('skipping this field, it has nans')
-            resampled_distribution = np.nan
+            fitted_density = np.nan
+            angles = np.nan
         else:
             print('I will analyze ' + field.session_id)
             resampled_distribution = resample_histogram(hd_histogram_field)
             fit = fit_von_mises_mixed_model(resampled_distribution)
-            get_model_fit_alpha = robj.r('get_model_fit_alpha')
-            get_model_fit_theta = robj.r('get_model_fit_theta')
-            alpha = get_model_fit_alpha(fit)
-            theta = get_model_fit_theta(fit)
-            get_estimated_density = robj.r('get_estimated_density')
-            estimated_density = get_estimated_density(fit)
-            plot_modes =robj.r("plot_modes")
-            # plot_modes(fit)
-            print(theta)  # these are the estimated modes (polar coordinates)
+            theta = get_model_fit_theta_value(fit)
+            angles = get_peaks_in_degrees(theta)
+            fitted_density = get_estimated_density_function(fit)
+        mode_angles.append(angles)
+        fitted_densities.append(fitted_density)
+    field_data['fitted_density'] = fitted_densities
+    field_data['mode_angles'] = mode_angles
+
     return field_data
 
 
