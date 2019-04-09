e1875a7bfe1333dbfcd1f94c357fe7ba9aa96b42
Klara Gerlei <klarizsofi@gmail.com>
2019-04-09 10:40:34 +0100

    resample histograms r code called from python

--- OverallAnalysis/count_modes_in_fields.py
+++ OverallAnalysis/count_modes_in_fields.py
@@ -5,13 +5,41 @@ from rpy2 import robjects as robj
 from rpy2.robjects import pandas2ri
 
 
-# count modes in firing fields
-robj.r.source('count_modes_circular_histogram.R')
+local_path = '/Users/s1466507/Dropbox/Edinburgh/grid_fields/analysis/field_modes/'
 
-number_of_times_to_sample = robj.r(1000)
-hd_cluster_r = robj.FloatVector([1, 1, 2, 3, 4, 5, 4, 3, 2, 1, 2, 3, 4, 5, 6, 7, 6, 5, 2, 1])  # bimodal
-# hd_cluster = pd.DataFrame({'hd': head_direction_histogram})
-# hd_cluster_r = pandas2ri.py2ri(hd_cluster)
-rejection_sampling_r = robj.r['rejection.sampling']
-resampled_distribution = rejection_sampling_r(number_of_times_to_sample, hd_cluster_r)
-print(resampled_distribution)
+
+def resample_histogram(histogram):
+    robj.r.source('count_modes_circular_histogram.R')
+    number_of_times_to_sample = robj.r(1000)
+    hd_cluster_r = robj.FloatVector(histogram)
+    rejection_sampling_r = robj.r['rejection.sampling']
+    resampled_distribution = rejection_sampling_r(number_of_times_to_sample, hd_cluster_r)
+    print(resampled_distribution)
+    return resampled_distribution
+
+
+def analyze_histograms(field_data):
+    for index, field in field_data.iterrows():
+        hd_histogram_field = field.normalized_hd_hist
+        # print(hd_histogram_field)
+        if np.isnan(hd_histogram_field).sum() > 0:
+            print('skipping this field, it has nans')
+            resampled_distribution = np.nan
+        else:
+            resampled_distribution = resample_histogram(hd_histogram_field)
+        # add resampled dist to field df
+    return field_data
+
+
+def process_circular_data():
+    print('I am loading the data frame that has the fields')
+    field_data = pd.read_pickle(local_path + 'field_data_modes.pkl')
+    field_data = analyze_histograms(field_data)
+
+
+def main():
+    process_circular_data()
+
+
+if __name__ == '__main__':
+    main()
