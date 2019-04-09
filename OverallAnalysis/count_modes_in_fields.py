60deb609ac3e9005577d02b027c584e40365075c
Klara Gerlei <klarizsofi@gmail.com>
2019-04-09 11:13:35 +0100

    calculate fit

--- OverallAnalysis/count_modes_in_fields.py
+++ OverallAnalysis/count_modes_in_fields.py
@@ -9,16 +9,23 @@ local_path = '/Users/s1466507/Dropbox/Edinburgh/grid_fields/analysis/field_modes
 
 
 def resample_histogram(histogram):
-    robj.r.source('count_modes_circular_histogram.R')
     number_of_times_to_sample = robj.r(1000)
     hd_cluster_r = robj.FloatVector(histogram)
     rejection_sampling_r = robj.r['rejection.sampling']
     resampled_distribution = rejection_sampling_r(number_of_times_to_sample, hd_cluster_r)
-    print(resampled_distribution)
     return resampled_distribution
 
 
+def fit_von_mises_mixed_model(resampled_distribution):
+    fit_von_mises_mix = robj.r('vMFmixture')
+    find_best_fit = robj.r('vMFmin')
+    fit = find_best_fit(fit_von_mises_mix(resampled_distribution))
+    print(fit)
+    return fit
+
+
 def analyze_histograms(field_data):
+    robj.r.source('count_modes_circular_histogram.R')
     for index, field in field_data.iterrows():
         hd_histogram_field = field.normalized_hd_hist
         # print(hd_histogram_field)
@@ -26,8 +33,10 @@ def analyze_histograms(field_data):
             print('skipping this field, it has nans')
             resampled_distribution = np.nan
         else:
+            print('I will analyze ' + field.session_id)
             resampled_distribution = resample_histogram(hd_histogram_field)
-        # add resampled dist to field df
+            fit = fit_von_mises_mixed_model(resampled_distribution)
+
     return field_data
 
 
