34df6d30e6f3936dc454ea4e8ca905859bab9ccf
Harry <harrydclark91@gmail.com>
2019-04-16 10:33:33 +0100

    added constraints to border field detection,
    added functions copied from open_field_firing_fields.py and modified to suit border field detection

--- PostSorting/open_field_border_cells.py
+++ PostSorting/open_field_border_cells.py
@@ -53,7 +53,8 @@ def process_border_data(spatial_firing):
         fig.tight_layout()
         plt.show()
 
-        firing_fields_cluster, _ = PostSorting.open_field_firing_fields.get_firing_field_data(spatial_firing, index, threshold=30)
+        #firing_fields_cluster, _ = PostSorting.open_field_firing_fields.get_firing_field_data(spatial_firing, index, threshold=30)
+        firing_fields_cluster, _ = get_firing_field_data(spatial_firing, index, threshold=30)
         firing_fields_cluster = fields2map(firing_fields_cluster, firing_rate_map)
         firing_fields_cluster = clip_fields_by_size(firing_fields_cluster, bin_size_cm=2.5)
         firing_fields_cluster = put_firing_rates_back(firing_fields_cluster, firing_rate_map)
@@ -236,14 +237,180 @@ def putative_border_fields_clip_by_firing_rate(firing_rate_map):
     return firing_rate_map
 
 
+'''
+Functions below taken from open_field_firing_fields,
+modified to fit the purposes of border field detection
+'''
+
+
+# return indices of neighbors of bin considering borders
+def find_neighbors(bin_to_test, max_x, max_y):
+    x = bin_to_test[0]
+    y = bin_to_test[1]
+
+    neighbors = [[x, y+1], [x, y-1], [x+1, y], [x-1, y]]
+
+    if x == max_x:
+        neighbors = [[x, y+1], [x, y-1], [x-1, y]]
+    if y == max_y:
+        neighbors = [[x, y-1], [x+1, y], [x-1, y]]
+    if x == max_x and y == max_y:
+        neighbors = [[x, y-1], [x-1, y]]
+    if x == 0:
+        neighbors = [[x, y+1], [x, y-1], [x+1, y]]
+    if y == 0:
+        neighbors = [[x, y+1], [x+1, y], [x-1, y]]
+    if x == 0 and y == 0:
+        neighbors = [[x, y+1], [x+1, y]]
+
+    if x == max_x and y == 0:
+        neighbors = [[x, y+1], [x-1, y]]
+
+    if y == max_y and x == 0:
+        neighbors = [[x, y-1], [x+1, y]]
+
+    return neighbors
+
+
+# return the masked rate map and change the neighbor's indices to 1 if they are above threshold
+def find_neighborhood(masked_rate_map, rate_map, firing_rate_of_max, threshold):
+    changed = False
+    threshold = firing_rate_of_max * threshold / 100
+
+    firing_field_bins = np.array(np.where(masked_rate_map > 0))
+    firing_field_bins = firing_field_bins.T
+
+    for bin_to_test in firing_field_bins:
+        masked_rate_map[bin_to_test[0], bin_to_test[1]] = 2
+        neighbors = find_neighbors(bin_to_test, max_x=(masked_rate_map.shape[0]-1), max_y=(masked_rate_map.shape[1]-1))
+        for neighbor in neighbors:
+            if masked_rate_map[neighbor[0], neighbor[1]] == 2:
+                continue
+
+            firing_rate = rate_map[neighbor[0], neighbor[1]]
+            if firing_rate >= threshold:
+                masked_rate_map[neighbor[0], neighbor[1]] = 1
+                changed = True
+
+    return masked_rate_map, changed
+
+
+# check if the detected field is big enough to be a firing field
+def test_if_field_is_big_enough(field_indices, max_pixels_in_field = 32):
+    number_of_pixels = len(field_indices)
+    if number_of_pixels > max_pixels_in_field:
+        return True
+    return False
+
+
+# this is to avoid identifying the whole rate map as a field
+def test_if_field_is_small_enough(field_indices, rate_map):
+    number_of_pixels_in_field = len(field_indices)
+    number_of_pixels_on_map = len(rate_map.flatten())
+    if number_of_pixels_in_field > 3*(number_of_pixels_on_map/4):    # if field is bigger than 3/4 of map, reject field
+        return False
+    else:
+        return True
+
+
+def get_field_edge_values(field_indices):
+    x_min = np.array(field_indices)[:, 0].min()
+    x_max = np.array(field_indices)[:, 0].max()
+    y_min = np.array(field_indices)[:, 1].min()
+    y_max = np.array(field_indices)[:, 1].max()
+    return x_min, x_max, y_min, y_max
+
+'''
+def test_if_field_is_not_too_spread_out(field_indices, rate_map):
+    x_min, x_max, y_min, y_max = get_field_edge_values(field_indices)
+    if (x_max - x_min) >= len(rate_map) / 2:
+        return False
+    if (y_max - y_min) >= len(rate_map) / 2:
+        return False
+    return True
+'''
+
+
+def ensure_the_field_does_not_have_a_hole_in_the_middle(field_indices):
+    x_min, x_max, y_min, y_max = get_field_edge_values(field_indices)
+    middle_x = x_max - int((x_max - x_min) / 2)
+    middle_y = y_max - int((y_max - y_min) / 2)
+    if [middle_x, middle_y] not in field_indices.tolist():
+        return False
+    return True
+
+
+# test if the firing rate of the detected local maximum is higher than average + std firing
+def test_if_highest_bin_is_high_enough(rate_map, highest_rate_bin):
+    flat_rate_map = rate_map.flatten()
+    rate_map_without_removed_fields = np.take(flat_rate_map, np.where(flat_rate_map >= 0))
+    average_rate = np.mean(rate_map_without_removed_fields)
+    std_rate = np.std(rate_map)
+
+    firing_rate_of_highest_bin = rate_map[highest_rate_bin[0], highest_rate_bin[1]]
+    if firing_rate_of_highest_bin < 0.1:
+        return False
+
+    if firing_rate_of_highest_bin > average_rate + std_rate:
+        return True
+    else:
+        return False
+
+
+# find indices for an individual firing field
+def find_current_maxima_indices(rate_map, threshold):
+    highest_rate_bin = np.unravel_index(rate_map.argmax(), rate_map.shape)
+    found_new = test_if_highest_bin_is_high_enough(rate_map, highest_rate_bin)
+    max_fr = rate_map[highest_rate_bin]
+    if found_new is False:
+        return None, found_new, None
+    # plt.imshow(rate_map)
+    # plt.scatter(highest_rate_bin[1], highest_rate_bin[0], marker='o', s=500, color='yellow')
+    masked_rate_map = np.full((rate_map.shape[0], rate_map.shape[1]), 0)
+    masked_rate_map[highest_rate_bin] = 1
+    changed = True
+    while changed:
+        masked_rate_map, changed = find_neighborhood(masked_rate_map, rate_map, rate_map[highest_rate_bin], threshold=threshold)
+
+    field_indices = np.array(np.where(masked_rate_map > 0)).T
+    found_new = test_if_field_is_big_enough(field_indices)
+    if found_new is False:
+        return None, found_new, None
+    found_new = test_if_field_is_small_enough(field_indices, rate_map)
+    if found_new is False:
+        return None, found_new, None
+    found_new = ensure_the_field_does_not_have_a_hole_in_the_middle(field_indices)
+    if found_new is False:
+        return None, found_new, None
+    return field_indices, found_new, max_fr
+
+
+# mark indices of firing fields that are already found (so we don't find them again)
+def remove_indices_from_rate_map(rate_map, indices):
+    for index in indices:
+        rate_map[index[0], index[1]] = -10
+    return rate_map
+
+
+# find firing fields and maximum firing rates for each field for a cluster
+def get_firing_field_data(spatial_firing, cluster, threshold):
+    firing_fields_cluster = []
+    max_firing_rates_cluster = []
+    rate_map = spatial_firing.firing_maps[cluster].copy()
+    found_new = True
+    while found_new:
+        field_indices, found_new, max_firing_rate = find_current_maxima_indices(rate_map, threshold=threshold)
+        if found_new:
+            firing_fields_cluster.append(field_indices)
+            max_firing_rates_cluster.append(max_firing_rate)
+            rate_map = remove_indices_from_rate_map(rate_map, field_indices)
+    return firing_fields_cluster, max_firing_rates_cluster
+
+
 #  this is here for testing
 def main():
     print('-------------------------------------------------------------')
     print('-------------------------------------------------------------')
-
-    # get_correlation_vector(np.array([[1, 1, 1, 1], [2, 2, 2, 9], [3, 3, 3, 3], [4, 4, 4, 4]]))
-    # firing_rate_map_matlab = np.genfromtxt('C:/Users/s1466507/Documents/Ephys/test_overall_analysis/M5_2018-03-06_15-34-44_of/matlab_rate_map.csv', delimiter=',')
-    # get_correlation_vector(firing_rate_map_matlab)
 
     spatial_firing = pd.read_pickle('/home/harry/Downloads/spatial_firing_.pkl')
     spatial_firing = process_border_data(spatial_firing)
