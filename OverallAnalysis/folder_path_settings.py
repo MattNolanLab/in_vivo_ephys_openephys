be1728f0984289f72f7a687869459fdcb375d863
Klara Gerlei <klarizsofi@gmail.com>
2019-04-18 12:43:47 +0100

    folder path settings

---
+++ OverallAnalysis/folder_path_settings.py
@@ -0,0 +1,17 @@
+server_path_mouse = '//ardbeg.mvm.ed.ac.uk/nolanlab/Klara/Open_field_opto_tagging_p038/'
+server_path_rat = '//ardbeg.mvm.ed.ac.uk/nolanlab/Klara/grid_field_analysis/moser_data/Sargolini/all_data/'
+analysis_path = '/Users/s1466507/Dropbox/Edinburgh/grid_fields/analysis/'
+
+
+def get_server_path_mouse():
+    return server_path_mouse
+
+
+def get_server_path_rat():
+    return server_path_rat
+
+
+def get_local_path():
+    return analysis_path
+
+
