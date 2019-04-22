0364f7ea1c312230d9fe8d85f3e9bc774c3b6ab1
Klara Gerlei <klarizsofi@gmail.com>
2019-04-22 11:44:19 +0100

    add local test recording path

--- OverallAnalysis/folder_path_settings.py
+++ OverallAnalysis/folder_path_settings.py
@@ -2,6 +2,7 @@
 server_path_mouse = '//cmvm.datastore.ed.ac.uk/cmvm/sbms/groups/mnolan_NolanLab/ActiveProjects/Klara/Open_field_opto_tagging_p038/'
 server_path_rat = '//ardbeg.mvm.ed.ac.uk/nolanlab/Klara/grid_field_analysis/moser_data/Sargolini/all_data/'
 analysis_path = '/Users/s1466507/Dropbox/Edinburgh/grid_fields/analysis/'
+local_test_recording_path = '/Users/s1466507/Documents/Ephys/recordings/M12_2018-04-10_14-22-14_of/MountainSort/'
 
 
 def get_server_path_mouse():
@@ -14,5 +15,9 @@ def get_server_path_rat():
 
 def get_local_path():
     return analysis_path
+
+
+def get_local_test_recording_path():
+    return local_test_recording_path
 
 
