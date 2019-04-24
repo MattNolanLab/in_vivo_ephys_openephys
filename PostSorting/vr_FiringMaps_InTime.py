2f3a7cff14d51a2e7f0acb54a97f9ea9f85960bf
Klara Gerlei <klarizsofi@gmail.com>
2019-04-24 08:23:03 +0100

    imported script from in_vivo_virtual_reality script that convolves spikes in time. Also imported SOURCE CODE from elephant statitics package for calculating the gaussian kernal

---
+++ PostSorting/vr_FiringMaps_InTime.py
@@ -0,0 +1,94 @@
+import warnings
+import numpy as np
+import matplotlib.pylab as plt
+import scipy.stats
+
+
+def nextpow2(x):
+    """ Return the smallest integral power of 2 that >= x """
+    n = 2
+    while n < x:
+        n = 2 * n
+    return n
+
+
+def fftkernel(x, w):
+    """
+    y = fftkernel(x,w)
+    Function `fftkernel' applies the Gauss kernel smoother to an input
+    signal using FFT algorithm.
+    Input argument
+    x:    Sample signal vector.
+    w: 	Kernel bandwidth (the standard deviation) in unit of
+    the sampling resolution of x.
+    Output argument
+    y: 	Smoothed signal.
+    MAY 5/23, 2012 Author Hideaki Shimazaki
+    RIKEN Brain Science Insitute
+    http://2000.jukuin.keio.ac.jp/shimazaki
+    Ported to Python: Subhasis Ray, NCBS. Tue Jun 10 10:42:38 IST 2014
+    """
+    L = len(x)
+    Lmax = L + 3 * w
+    n = nextpow2(Lmax)
+    X = np.fft.fft(x, n)
+    f = np.arange(0, n, 1.0) / n
+    f = np.concatenate((-f[:int(n / 2)], f[int(n / 2):0:-1]))
+    K = np.exp(-0.5 * (w * 2 * np.pi * f)**2)
+    y = np.fft.ifft(X * K, n)
+    y = y[:L].copy()
+    return y
+
+
+"""
+## Here we convolve spikes in time trial by trial basis on clusters
+1. load data:
+    data is firing times in ms
+2. bin spikes into 250 ms windows
+"""
+
+
+def generate_time_bins(spike_times):
+    time_bins = np.arange(0,np.max(spike_times),7500)
+    number_of_bins = time_bins.shape[0]
+    return time_bins
+
+
+def bin_spike_times(spike_times, number_of_bins):
+    binned_spikes_in_time = create_histogram(spike_times, number_of_bins)
+    return binned_spikes_in_time
+
+
+def create_histogram(spike_times, number_of_bins):
+    posrange = np.linspace(number_of_bins.min(), number_of_bins.max(),  num=max(number_of_bins)+1)
+    values = np.array([[posrange[0], posrange[-1]]])
+    H, bins = np.histogram(spike_times, bins=(posrange), range=values)
+    return H
+
+
+def convolve_binned_spikes(binned_spike_times):
+    convolved_spikes=[]
+    convolved_spikes = elephant.statistics.fftkernel(binned_spike_times, 2)
+    return convolved_spikes
+
+
+def convolve_spikes_in_time(spike_data):
+    print('I am convolving spikes in time...')
+    for cluster in range(len(spike_data)):
+        print(spike_data.at[cluster, "session_id"], cluster)
+        spike_times = np.array(spike_data.at[cluster, "firing_times"])
+        number_of_bins = generate_time_bins(spike_times)
+        binned_spike_times = bin_spike_times(spike_times, number_of_bins)
+        convolved_spikes = convolve_binned_spikes(binned_spike_times)
+    return spike_data
+
+
+def convolve_speed_in_time(spike_data):
+    print('I am convolving spikes in time...')
+    for cluster in range(len(spike_data)):
+        print(spike_data.at[cluster, "session_id"], cluster)
+        spike_times = np.array(spike_data.at[cluster, "speed_per200ms"])
+        number_of_bins = generate_time_bins(spike_times)
+        binned_speed = bin_spike_times(spike_times, number_of_bins)
+        convolved_speed = convolve_binned_spikes(binned_speed)
+    return spike_data
