9afec9c66f3177dbcc4f3014e3faf40f2612b474
Klara Gerlei <klarizsofi@gmail.com>
2019-05-01 10:33:17 +0100

    change plot

--- OverallAnalysis/field_analysis_two_sample_watson.py
+++ OverallAnalysis/field_analysis_two_sample_watson.py
@@ -141,16 +141,22 @@ def plot_histogram_of_watson_stat(field_data, type='all', animal='mouse'):
     if type == 'grid':
         grid_cells = field_data['cell type'] == 'grid'
         watson_stats_accepted_fields = field_data.watson_two_stat[field_data.accepted_field & grid_cells]
+        watson_shuffled = field_data.watson_stat_shuffled[field_data.accepted_field & grid_cells]
     elif type == 'nc':
         not_classified = field_data['cell type'] == 'na'
         watson_stats_accepted_fields = field_data.watson_two_stat[field_data.accepted_field & not_classified]
+        watson_shuffled = field_data.watson_stat_shuffled[field_data.accepted_field & not_classified]
     else:
         watson_stats_accepted_fields = field_data.watson_two_stat[field_data.accepted_field]
+        watson_shuffled = field_data.watson_stat_shuffled[field_data.accepted_field]
 
     fig, ax = plt.subplots()
-    plt.hist(watson_stats_accepted_fields, bins=20, color='navy')
+    plt.hist(watson_stats_accepted_fields, bins=20, color='navy', alpha=0.7, normed=True)
+    plt.hist(watson_shuffled.values.flatten()[0], bins=20, color='grey', alpha=0.7, normed=True)
     ax.xaxis.set_tick_params(labelsize=20)
     ax.yaxis.set_tick_params(labelsize=20)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
     print('Number of ' + type + ' fields in ' + animal + ': ' + str(len(watson_stats_accepted_fields)))
     print('p < 0.01 for ' + str((watson_stats_accepted_fields > 0.268).sum()))
 
