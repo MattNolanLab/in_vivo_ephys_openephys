2ff002d85689dd7221f49118df2434b4b0c700c4
Klara Gerlei <klarizsofi@gmail.com>
2019-05-31 14:41:36 +0100

    plot results

--- OverallAnalysis/compare_hd_with_expected_hd.py
+++ OverallAnalysis/compare_hd_with_expected_hd.py
@@ -15,6 +15,7 @@ from rpy2.robjects import pandas2ri
 import scipy.stats
 import seaborn
 import PostSorting.compare_first_and_second_half
+import PostSorting.open_field_head_direction
 import PostSorting.parameters
 
 prm = PostSorting.parameters.Parameters()
@@ -83,15 +84,37 @@ def load_field_data(output_path, server_path, spike_sorter, animal):
     return field_data_combined
 
 
+# get head-direction hist from bins of field
+def get_hd_in_field_spikes(rate_map_indices, spatial_data, prm):
+    hd_in_field_hist = np.zeros((len(rate_map_indices), 360))
+    for index, bin_in_field in enumerate(rate_map_indices):
+        inside_bin = PostSorting.open_field_head_direction.get_indices_for_bin(bin_in_field, spatial_data, prm)
+        hd = inside_bin.hd.values + 180
+        hd_hist = np.histogram(hd, bins=360, range=(0, 360))[0]
+        # hd_hist = PostSorting.open_field_head_direction.get_hd_histogram(hd)
+        hd_in_field_hist[index] = hd_hist
+    return hd_in_field_hist
+
+
+def get_rate_map_values_for_bins(rate_map_indices, rate_map):
+    rates = np.zeros((len(rate_map_indices), 1))
+    for index, bin_in_field in enumerate(rate_map_indices):
+        rate = rate_map[bin_in_field[0], bin_in_field[1]]
+        rates[index] = rate
+    return rates
+
+
 def process_data(animal):
     if animal == 'mouse':
         output_path = local_path + 'mouse_data.pkl'
         server_path = server_path_mouse
         spike_sorter = '/MountainSort'
+        prm.set_pixel_ratio(440)
     else:
         server_path = server_path_rat
         spike_sorter = ''
         output_path = local_path + 'rat_data.pkl'
+        prm.set_pixel_ratio(1)
     field_data = load_field_data(output_path, server_path, spike_sorter, animal)
     for index, field in field_data.iterrows():
         spatial_data_field = pd.DataFrame()
@@ -99,13 +122,38 @@ def process_data(animal):
         spatial_data_field['y'] = field.position_y_pixels
         spatial_data_field['hd'] = field.hd
         spatial_data_field['synced_time'] = field.synced_time
-        print(field)
+        rate_map_indices = field.indices_rate_map
+        hd_in_field_histograms = get_hd_in_field_spikes(rate_map_indices, spatial_data_field, prm)
+        rates_for_bins = get_rate_map_values_for_bins(rate_map_indices, field.rate_map.iloc[0])
+        weighed_hists = hd_in_field_histograms * rates_for_bins
+        weighed_hist_sum = np.sum(weighed_hists, axis=0)
+        hd_dist_session = (field.hd_in_field_session * 180 / np.pi)
+        hd_hist_session = np.histogram(hd_dist_session, bins=360, range=(0, 360))[0]
+        hd_hist_normed_estimate = np.nan_to_num(weighed_hist_sum / hd_hist_session)
+        estimate_smooth = PostSorting.open_field_head_direction.get_rolling_sum(hd_hist_normed_estimate, window=23)
+        estimate_smooth /= 23
 
+        hd_spikes_real_hist = PostSorting.open_field_head_direction.get_hd_histogram(field.hd_in_field_spikes)
+        hd_session_real_hist = PostSorting.open_field_head_direction.get_hd_histogram(field.hd_in_field_session)
+        norm_hist_real = np.nan_to_num(hd_spikes_real_hist / hd_session_real_hist)
+        plt.cla()
+        # plt.scatter(range(0, 360), hd_hist_normed_estimate, color='red', alpha=0.3, normed=True)
+        # plt.plot(hd_hist_normed_estimate, color='red')
+        max_est = max(hd_hist_normed_estimate)
+        max_real = max(norm_hist_real)
+        scale = max_real / max_est
+        fig, ax = plt.subplots()
+        ax.plot(norm_hist_real, color='red', alpha=0.4, label='real', linewidth=5)
+        ax.plot(estimate_smooth * scale, color='black', alpha=0.4, label='estimate', linewidth=5)
+        plt.title('hd ' + str(round(field.hd_score, 1)) + ' grid ' + str(round(field.grid_score, 1)))
+        legend = plt.legend()
+        legend.get_frame().set_facecolor('none')
+        plt.savefig(local_path + animal + field.session_id + str(field.cluster_id) + str(field.field_id) + 'estimated_hd_rate_vs_real.png')
 
 
 def main():
     process_data('mouse')
-    process_data('rat')
+    # process_data('rat')
 
 
 if __name__ == '__main__':
