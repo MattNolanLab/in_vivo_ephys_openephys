876423c6e3b743ecc05fae385010b1c8c428853c
Klara Gerlei <klarizsofi@gmail.com>
2019-06-21 10:35:56 +0100

    analyze simulated data

--- OverallAnalysis/shuffle_cell_analysis.py
+++ OverallAnalysis/shuffle_cell_analysis.py
@@ -699,19 +699,19 @@ def main():
     # spatial_firing_all_mice = load_data_frame_spatial_firing(local_path_mouse, server_path_mouse, spike_sorter='/MountainSort')
     # spatial_firing_all_rats = load_data_frame_spatial_firing(local_path_rat, server_path_rat, spike_sorter='')
 
-    # spatial_firing_all_simulated = load_data_frame_spatial_firing(local_path_simulated, server_path_simulated + 'ventral/', spike_sorter='', df_path='')
-    # prm.set_pixel_ratio(100)
-    # process_data(spatial_firing_all_rats, 50, animal='rat', shuffle_type='distributive')
-    # prm.set_pixel_ratio(440)
-    # process_data(spatial_firing_all_mice, 30, animal='mouse', shuffle_type='distributive')
-    # process_data(spatial_firing_all_simulated, 1000, animal='simulated', shuffle_type='distributive')
-
-    spatial_firing_all_simulated = load_data_frame_spatial_firing(local_path_simulated, server_path_simulated + 'control/', spike_sorter='', df_path='')
+    spatial_firing_all_simulated = load_data_frame_spatial_firing(local_path_simulated, server_path_simulated + 'ventral_5/', spike_sorter='', df_path='')
     prm.set_pixel_ratio(100)
     # process_data(spatial_firing_all_rats, 50, animal='rat', shuffle_type='distributive')
     # prm.set_pixel_ratio(440)
     # process_data(spatial_firing_all_mice, 30, animal='mouse', shuffle_type='distributive')
-    process_data(spatial_firing_all_simulated, 1000, animal='simulated', shuffle_type='distributive_control')
+    process_data(spatial_firing_all_simulated, 1000, animal='simulated', shuffle_type='distributive_5')
+
+    spatial_firing_all_simulated = load_data_frame_spatial_firing(local_path_simulated, server_path_simulated + 'control_5/', spike_sorter='', df_path='')
+    prm.set_pixel_ratio(100)
+    # process_data(spatial_firing_all_rats, 50, animal='rat', shuffle_type='distributive')
+    # prm.set_pixel_ratio(440)
+    # process_data(spatial_firing_all_mice, 30, animal='mouse', shuffle_type='distributive')
+    process_data(spatial_firing_all_simulated, 1000, animal='simulated', shuffle_type='distributive_control_5')
 
 
 if __name__ == '__main__':
