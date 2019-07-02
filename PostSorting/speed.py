de6470792e1750f4cd691508a869fb01c68bd5d0
Klara Gerlei <klarizsofi@gmail.com>
2019-07-02 21:01:25 +0100

    comment

--- PostSorting/speed.py
+++ PostSorting/speed.py
@@ -9,6 +9,7 @@ def calculate_speed_score(position, spatial_firing, sigma=8):
     for index, cell in spatial_firing.iterrows():
         firing_times = cell.firing_times
         firing_hist, edges = np.histogram(firing_times, bins=len(speed))
+        # todo this should be firing rates not just histograms
         smooth_hist = scipy.ndimage.filters.gaussian_filter(firing_hist, sigma)
         plt.plot(speed)
         plt.plot(np.histogram(smooth_hist, bins=len(speed))[0])
