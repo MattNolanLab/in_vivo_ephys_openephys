b2f34702b77920882b37a25ca07c2ba392dccb4d
Klara Gerlei <klarizsofi@gmail.com>
2019-08-09 22:38:13 +0100

    iloc

--- PostSorting/open_field_make_plots.py
+++ PostSorting/open_field_make_plots.py
@@ -246,9 +246,9 @@ def plot_hd_for_firing_fields(spatial_firing, spatial_data, prm):
 
 
 def plot_spikes_not_in_fields(spatial_firing, cluster, spatial_firing_cluster, of_plot):
-    all_spikes_in_fields = np.hstack(np.array(spatial_firing.spike_times_in_fields[cluster]))
-    mask_for_spikes_not_in_fields = ~np.in1d(spatial_firing.firing_times[cluster], all_spikes_in_fields)
-    spike_times_not_in_fields = spatial_firing.firing_times[cluster][mask_for_spikes_not_in_fields]
+    all_spikes_in_fields = np.hstack(np.array(spatial_firing.spike_times_in_fields.iloc[cluster]))
+    mask_for_spikes_not_in_fields = ~np.in1d(spatial_firing.firing_times.iloc[cluster], all_spikes_in_fields)
+    spike_times_not_in_fields = spatial_firing.firing_times.iloc[cluster][mask_for_spikes_not_in_fields]
     not_in_fields_df = spatial_firing_cluster.loc[spatial_firing_cluster['firing_times'].isin(spike_times_not_in_fields)]
     of_plot.scatter(not_in_fields_df['x'].values, not_in_fields_df['y'].values, color='black', marker='o', s=6)
 
