d0e536f0ddb0f765b27945fd4cbc1423cd969069
Sarah Tennant <stennan2@exseed.ed.ac.uk>
2019-08-20 12:19:58 +0100

    added outline functions and code explanation

--- PostSorting/vr_stopping_strategy.py
+++ PostSorting/vr_stopping_strategy.py
@@ -1,1 +1,24 @@
 
+## The aim of this code is to seperate trials in a vr session into what strategy the animal is using:
+# 1. run throughs                       / stops =0
+# 2. inefficient successful strategy    / stops > 0 in and outside RZ  
+# 3. efficient successful strategy      / stops > 0 in RZ
+# 4. failing                            / stops > 0 outside RZ
+
+
+# the output of this code is an array, the same size as binned data (trials x location bins) with a tag as to what strategy is used on that trial
+
+
+
+
+def aquire_stoping_data():
+  
+  
+  
+def split_trials_on_strategy():
+  
+  
+  
+def seperate_trials_by_strategy():
+  aquire_stopping_data()
+  split_trials_on_strategy()
