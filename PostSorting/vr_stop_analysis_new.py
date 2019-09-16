09cea651d3f0d2a1e965c14dc0417a4ce9323275
Sarah Tennant <stennan2@exseed.ed.ac.uk>
2019-09-16 11:53:33 +0100

    Update vr_stop_analysis_new.py

--- PostSorting/vr_stop_analysis_new.py
+++ PostSorting/vr_stop_analysis_new.py
@@ -46,7 +46,6 @@ def get_stop_locations(raw_position_data, stop_threshold):
     
     stops = remove_extra_stops(5, stop_locs)
     #stops = np.hstack((stop_locs, stop_trials, stop_types))
-
     return stop_locs, stop_trials, stop_types
     
     
@@ -64,4 +63,56 @@ def calculate_stop_data_from_parameters(raw_position_data, processed_position_da
     processed_position_data['stop_trial_type'] = pd.Series(stop_trial_types)
     return processed_position_data
 
+
+def find_rewarded_positions_test(raw_position_data,processed_position_data):
+    stop_locations = np.array(processed_position_data['stop_location_cm'])
+    stop_trials = np.array(processed_position_data['stop_trial_number'])
+    rewarded_stop_locations=[]
+    rewarded_trials=[]
+    for tcount, trial in enumerate(np.unique(stop_trials)):
+            trial_locations = np.take(stop_locations, np.where(stop_trials == trial)[0])
+            if len(trial_locations) > 0:
+                for count in trial_locations:
+                    if count >= 90 and count <= 110:
+                        rewarded_stop_locations= np.append(rewarded_stop_locations, count)
+                        rewarded_trials=np.append(rewarded_trials, trial)
+                        break
+    processed_position_data['rewarded_stop_locations'] = pd.Series(rewarded_stop_locations)
+    processed_position_data['rewarded_trials'] = pd.Series(rewarded_trials)
+    return processed_position_data
     
+def get_bin_size(spatial_data):
+    #bin_size_cm = 1
+    track_length = spatial_data.x_position_cm.max()
+    start_of_track = spatial_data.x_position_cm.min()
+    #number_of_bins = (track_length - start_of_track)/bin_size_cm
+    number_of_bins = 200
+    bin_size_cm = (track_length - start_of_track)/number_of_bins
+    bins = np.arange(start_of_track,track_length, 200)
+    return bin_size_cm,number_of_bins, bins
+
+
+def calculate_average_stops(raw_position_data,processed_position_data):
+    stop_locations = processed_position_data.stop_location_cm.values
+    stop_locations = stop_locations[~np.isnan(stop_locations)] #need to deal with
+    bin_size_cm,number_of_bins, bins = get_bin_size(raw_position_data)
+    number_of_trials = raw_position_data.trial_number.max() # total number of trials
+    stops_in_bins = np.zeros((len(range(int(number_of_bins)))))
+    for loc in range(int(number_of_bins)-1):
+        stops_in_bin = len(stop_locations[np.where(np.logical_and(stop_locations > (loc), stop_locations <= (loc+1)))])/number_of_trials
+        stops_in_bins[loc] = stops_in_bin
+
+    processed_position_data['average_stops'] = pd.Series(stops_in_bins)
+    processed_position_data['position_bins'] = pd.Series(range(int(number_of_bins)))
+    return processed_position_data
+
+
+
+
+def process_stops(raw_position_data,processed_position_data, prm):
+    processed_position_data = calculate_stops(raw_position_data, processed_position_data, 10.7)
+    processed_position_data = calculate_average_stops(raw_position_data,processed_position_data)
+    gc.collect()
+    processed_position_data = find_rewarded_positions_test(raw_position_data,processed_position_data)
+    return processed_position_data
+
