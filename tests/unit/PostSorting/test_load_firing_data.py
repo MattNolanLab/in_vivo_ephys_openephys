587fab483303f427410f4a2b1ee39b9da6861a83
klaragerlei <klarizsofi@gmail.com>
2019-09-24 12:46:05 +0100

    fix data type of test data and numbers

--- tests/unit/PostSorting/test_load_firing_data.py
+++ tests/unit/PostSorting/test_load_firing_data.py
@@ -4,9 +4,10 @@ import PostSorting.load_firing_data
 
 def test_correct_detected_ch_for_dead_channels():
     dead_channels = [1, 2]
-    primary_channels = [1, 1, 3, 6, 7, 9, 11, 3]
+    dead_channels = list(map(int, dead_channels))
+    primary_channels = np.array([1, 1, 3, 6, 7, 9, 11, 3])
 
-    desired_result = [2, 2, 5, 7, 9, 11, 13, 5]
+    desired_result = [3, 3, 5, 8, 9, 11, 13, 5]
     result = PostSorting.load_firing_data.correct_detected_ch_for_dead_channels(dead_channels, primary_channels)
 
     assert np.allclose(result, desired_result, rtol=1e-05, atol=1e-08)
