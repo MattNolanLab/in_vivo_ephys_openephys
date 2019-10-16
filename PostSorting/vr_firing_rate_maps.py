fixed speed in stop detection, implemented moving average for rolling 200ms window.
removed bad cluster plotting for now

PostSorting/vr_firing_rate_maps.py
@@ -21,11 +21,10 @@ def get_bin_size(spatial_data):
 '''
 
 def get_bin_size(spatial_data):
-    track_length = spatial_data.x_position_cm.max()
-    start_of_track = spatial_data.x_position_cm.min()
+    track_length = spatial_data.x_position_cm.max() - spatial_data.x_position_cm.min()
     number_of_bins = 200
-    bin_size_cm = (track_length - start_of_track)/number_of_bins
-    return bin_size_cm,number_of_bins
+    bin_size_cm = track_length/number_of_bins
+    return bin_size_cm, number_of_bins
 
 
 def gaussian_kernel(kernx):
@@ -207,7 +206,7 @@ def make_firing_field_maps_all(spike_data, raw_position_data, processed_position
         firing_rate_map = add_trial_type(firing_rate_map, processed_position_data)
         firing_rate_map = normalise_spike_number_by_time_all(firing_rate_map, processed_position_data.binned_time_ms_per_trial)
         firing_rate_map = smooth_spike_rate(firing_rate_map)
-        plot_rate_data(prm, firing_rate_map, spike_data, np.max(array_of_trials), cluster_index)
+        plot_rate_data(     prm, firing_rate_map, spike_data, np.max(array_of_trials), cluster_index)
         plot_rate_norm_data(prm, firing_rate_map, spike_data, np.max(array_of_trials), cluster_index)
         #firing_rate_map = gaussian_convolve_spike_rate(firing_rate_map)
         spike_data = add_data_to_dataframe(cluster_index, firing_rate_map, spike_data)

PostSorting/vr_spatial_data.py
@@ -76,7 +76,7 @@ def bin_data_trial_by_trial(raw_position_data,processed_position_data):
     dwell_time_per_sample = np.array(raw_position_data['dwell_time_ms'])
     time_per_sample = np.array(raw_position_data['time_seconds'])
 
-    for t in range(1,int(number_of_trials)):
+    for t in range(1,int(number_of_trials+1)):
         trial_locations = np.take(locations, np.where(trials == t)[0])
         trial_type = int(stats.mode(np.take(trial_types, np.where(trials == t)[0]))[0])
         for loc in range(int(number_of_bins)):

PostSorting/vr_sync_spatial_data.py
@@ -281,6 +281,13 @@ def moving_average(a, n):
     ret[n:] = ret[n:] - ret[:-n]
     return ret[n:] / n
 
+
+def running_mean(a, N):
+    # returns same length array as a, as a running mean with a
+    # window n, final values are backed with backend values
+    cumsum = np.cumsum(np.insert(a, 0, 0), dtype=float)
+    return np.append(((cumsum[N:] - cumsum[:-N]) / N), a[-N+1:])
+
 '''
 Calculates moving average
 
@@ -327,7 +334,7 @@ def get_avg_speed_200ms(position_data, prm):
     print('Calculating average speed...')
     velocity = np.array(position_data['velocity'])  # Get the raw location from the movement channel
     sampling_points_per200ms = int(prm.get_sampling_rate()/5)
-    position_data['speed_per200ms'] = get_rolling_sum(velocity, sampling_points_per200ms)# Calculate average speed at each point by averaging instant velocities
+    position_data['speed_per200ms'] = running_mean(velocity, sampling_points_per200ms) # Calculate average speed at each point by averaging instant velocities
     return position_data
 
 