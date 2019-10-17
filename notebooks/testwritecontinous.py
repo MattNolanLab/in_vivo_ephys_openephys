fdfb9f539503a9b976961a446a38e25b83186cac
Teris Tam <teristam@gmail.com>
2019-10-17 20:27:55 +0100

    Add in more comments and make the script compatible with Python 3.6

--- notebooks/testwritecontinous.py
+++ notebooks/testwritecontinous.py
@@ -3,18 +3,16 @@ import OpenEphys
 import numpy as np
 import os
 import shutil
-#%%
+#%% Example: Verify that the data is written correctly
 file = OpenEphys.loadContinuousFast('testData/M1_D31_2018-11-01_12-28-25/100_CH1.continuous')
 OpenEphys.writeContinuousFile('test.continuous',file['header'],file['timestamps'],file['data'],file['recordingNumber'])
 x = OpenEphys.loadContinuousFast('test.continuous')
-#%%
-
 np.allclose(file['data'],x['data'])
 
-#%% create short files for debugging purpose
+#%% Example: create short files of an entire recording for debugging purpose
 basePath = 'testData/M1_D31_2018-11-01_12-28-25'
 newPath = basePath+'_short'
-shortenLength = 30000*60*5
+shortenLength = 30000*60*5 #length of the shorten data
 
 try:
     os.mkdir(newPath,)
@@ -23,15 +21,17 @@ except  FileExistsError:
 
 for f in os.scandir(basePath):
     if f.name.endswith('.continuous'):
-        print(f'{f.name} truncated')
+        # Truncate the continous files
+        print(f'{0} truncated'.format(f.name))
         file = OpenEphys.loadContinuousFast(f.path)
         OpenEphys.writeContinuousFile(newPath+'/'+f.name, file['header'],
             file['timestamps'], file['data'][:shortenLength], file['recordingNumber'])
     else:
+        # Copy all other files
         if f.is_dir():
             shutil.copytree(f.path, newPath+'/'+f.name)
         else:
             shutil.copyfile(f.path, newPath+'/'+f.name)
 
-        print(f'{f.name} copied')
+        print(f'{0} copied'.format(f.name))
 
