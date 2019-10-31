fixed speed to 0-20 cm/s for normalising color to speed in binned speed plot
added speed_trialtypes to speed anaylsis

reimplemented calculate stops based on binned speed in speed anaylsis. and commented out old stop calculates for now

PostSorting/vr_cued_make_plots.py
@@ -564,7 +564,7 @@ def plot_binned_velocity(raw_position_data, processed_position_data, prm, plot_b
     min_speed = np.nanmin(np.concatenate(list(processed_position_data.speed_trials_binned[:int(max(processed_position_data.speed_trial_numbers))])).ravel())
 
     # https://stackoverflow.com/questions/10533929/colors-of-rectangles-in-python
-    normal = pl.Normalize(0, max_speed)
+    normal = pl.Normalize(0, 20) # 0 to 25cm/s
     cax, _ = cbar.make_axes(ax)
     cb2 = cbar.ColorbarBase(cax, cmap=pl.cm.jet, norm=normal)
     cax.set_ylabel('Speeds (cm/s)', fontsize=12, labelpad=10)