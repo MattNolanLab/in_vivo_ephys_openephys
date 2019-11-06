c5cc16ad237dd99e18d044edb825fe6eb1c19bf8
Klara Gerlei <klarizsofi@gmail.com>
2019-11-06 18:07:55 +0000

    print firing rates of example fields

---
+++ OverallAnalysis/example_get_firing_rates_for_half_fields.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+
+analysis_path = '/Users/s1466507/Dropbox/Edinburgh/grid_fields/analysis/'
+analysis_path = analysis_path + 'firing_rates_for_plots/'
+
+
+first_path = analysis_path + 'first/'
+second_path = analysis_path + 'second/'
+
+spatial_first = pd.read_pickle(first_path + 'spatial_firing.pkl')
+spatial_second = pd.read_pickle(second_path + 'spatial_firing.pkl')
+
+cell_first = spatial_first.iloc[0]
+cell_second = spatial_second.iloc[0]
+
+print(' ')
+print('fields in first half')
+for field in range(len(cell_first.firing_fields_hd_cluster)):
+    hd_hist_cluster = cell_first.firing_fields_hd_cluster[field]
+    hd_hist_session = cell_first.firing_fields_hd_session[field] / 30000
+    hd_hist = hd_hist_cluster / hd_hist_session / 1000
+    max_firing_rate = np.max(hd_hist[~np.isnan(hd_hist)].flatten())
+    print(max_firing_rate)
+
+
+print('fields in second half')
+for field in range(len(cell_second.firing_fields_hd_cluster)):
+    hd_hist_cluster = cell_second.firing_fields_hd_cluster[field]
+    hd_hist_session = cell_second.firing_fields_hd_session[field] / 30000
+    hd_hist = hd_hist_cluster / hd_hist_session / 1000
+    max_firing_rate = np.max(hd_hist[~np.isnan(hd_hist)].flatten())
+    print(max_firing_rate)
+
+
+
+
