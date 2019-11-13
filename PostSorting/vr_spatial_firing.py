78f407e349601249d886d3afa3bf857451af7db2
hdclark94 <Worldmap365>
2019-11-13 20:48:27 +0000

    added split_spatial_firing_by_trial_type_cued and removed try except!

--- PostSorting/vr_spatial_firing.py
+++ PostSorting/vr_spatial_firing.py
@@ -127,22 +127,48 @@ def split_spatial_firing_by_trial_type(spike_data):
         locations = np.array(cluster_df['x_position_cm'].tolist())
         trial_type = np.array(cluster_df['trial_type'].tolist())
         # index out of range error in following line
-        try:
-            beaconed_locations = np.take(locations, np.where(trial_type == 0)[1]) #split location and trial number
-            nonbeaconed_locations = np.take(locations,np.where(trial_type == 1)[1])
-            probe_locations = np.take(locations, np.where(trial_type == 2)[1])
-            beaconed_trials = np.take(trials, np.where(trial_type == 0)[1])
-            nonbeaconed_trials = np.take(trials, np.where(trial_type == 1)[1])
-            probe_trials = np.take(trials, np.where(trial_type == 2)[1])
+        #try:
+        beaconed_locations = np.take(locations, np.where(trial_type == 0)[1]) #split location and trial number
+        nonbeaconed_locations = np.take(locations,np.where(trial_type == 1)[1])
+        probe_locations = np.take(locations, np.where(trial_type == 2)[1])
+        beaconed_trials = np.take(trials, np.where(trial_type == 0)[1])
+        nonbeaconed_trials = np.take(trials, np.where(trial_type == 1)[1])
+        probe_trials = np.take(trials, np.where(trial_type == 2)[1])
 
-            spike_data.at[cluster_index, 'beaconed_position_cm'] = list(beaconed_locations)
-            spike_data.at[cluster_index, 'beaconed_trial_number'] = list(beaconed_trials)
-            spike_data.at[cluster_index, 'nonbeaconed_position_cm'] = list(nonbeaconed_locations)
-            spike_data.at[cluster_index, 'nonbeaconed_trial_number'] = list(nonbeaconed_trials)
-            spike_data.at[cluster_index, 'probe_position_cm'] = list(probe_locations)
-            spike_data.at[cluster_index, 'probe_trial_number'] = list(probe_trials)
-        except IndexError:
-            continue
+        spike_data.at[cluster_index, 'beaconed_position_cm'] = list(beaconed_locations)
+        spike_data.at[cluster_index, 'beaconed_trial_number'] = list(beaconed_trials)
+        spike_data.at[cluster_index, 'nonbeaconed_position_cm'] = list(nonbeaconed_locations)
+        spike_data.at[cluster_index, 'nonbeaconed_trial_number'] = list(nonbeaconed_trials)
+        spike_data.at[cluster_index, 'probe_position_cm'] = list(probe_locations)
+        spike_data.at[cluster_index, 'probe_trial_number'] = list(probe_trials)
+        #except IndexError:
+        #    continue
+    return spike_data
+
+def split_spatial_firing_by_trial_type_cued(spike_data):
+    print('I am splitting firing locations by trial type...')
+    for cluster_index in range(len(spike_data)):
+        cluster_index = spike_data.cluster_id.values[cluster_index] - 1
+        cluster_df = spike_data.loc[[cluster_index]] # dataframe for that cluster
+        trials = np.array(cluster_df['trial_number'].tolist())
+        locations = np.array(cluster_df['x_position_cm_offset'].tolist())
+        trial_type = np.array(cluster_df['trial_type'].tolist())
+        # index out of range error in following line
+
+        beaconed_locations = np.take(locations, np.where(trial_type == 0)[1]) #split location and trial number
+        nonbeaconed_locations = np.take(locations,np.where(trial_type == 1)[1])
+        probe_locations = np.take(locations, np.where(trial_type == 2)[1])
+        beaconed_trials = np.take(trials, np.where(trial_type == 0)[1])
+        nonbeaconed_trials = np.take(trials, np.where(trial_type == 1)[1])
+        probe_trials = np.take(trials, np.where(trial_type == 2)[1])
+
+        spike_data.at[cluster_index, 'beaconed_position_cm'] = list(beaconed_locations)
+        spike_data.at[cluster_index, 'beaconed_trial_number'] = list(beaconed_trials)
+        spike_data.at[cluster_index, 'nonbeaconed_position_cm'] = list(nonbeaconed_locations)
+        spike_data.at[cluster_index, 'nonbeaconed_trial_number'] = list(nonbeaconed_trials)
+        spike_data.at[cluster_index, 'probe_position_cm'] = list(probe_locations)
+        spike_data.at[cluster_index, 'probe_trial_number'] = list(probe_trials)
+
     return spike_data
 
 
