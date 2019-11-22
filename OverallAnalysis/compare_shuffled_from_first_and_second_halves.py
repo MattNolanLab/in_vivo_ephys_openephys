237d4a431764784c19ee668cd1444bd34f6c024b
Klara Gerlei <klarizsofi@gmail.com>
2019-11-22 18:53:48 +0000

    get all corr values

--- OverallAnalysis/compare_shuffled_from_first_and_second_halves.py
+++ OverallAnalysis/compare_shuffled_from_first_and_second_halves.py
@@ -8,6 +8,7 @@ import OverallAnalysis.shuffle_cell_analysis
 import PostSorting.compare_first_and_second_half
 import PostSorting.open_field_firing_maps
 import PostSorting.parameters
+from scipy import signal
 
 prm = PostSorting.parameters.Parameters()
 
@@ -127,7 +128,7 @@ def split_in_two(cell):
 
     second = pd.DataFrame()
     second['session_id'] = cell.session_id.iloc[0]
-    first['cluster_id'] = cell.cluster_id.iloc[0]
+    second['cluster_id'] = cell.cluster_id.iloc[0]
     second['number_of_spikes'] = [len(spike_data_cluster_second.firing_times)]
     second['firing_times'] = [spike_data_cluster_second.firing_times]
     second['position_x'] = [spike_data_cluster_second.position_x]
@@ -136,10 +137,10 @@ def split_in_two(cell):
     second['position_y_pixels'] = [spike_data_cluster_second.position_y_pixels]
     second['hd'] = [spike_data_cluster_second.hd]
 
-    second['trajectory_x'] = [synced_spatial_data_second_half.position_x]
-    second['trajectory_y'] = [synced_spatial_data_second_half.position_y]
-    second['trajectory_hd'] = [synced_spatial_data_second_half.hd]
-    second['trajectory_times'] = [synced_spatial_data_second_half.synced_time]
+    second['trajectory_x'] = [synced_spatial_data_second_half.position_x.reset_index(drop=True)]
+    second['trajectory_y'] = [synced_spatial_data_second_half.position_y.reset_index(drop=True)]
+    second['trajectory_hd'] = [synced_spatial_data_second_half.hd.reset_index(drop=True)]
+    second['trajectory_times'] = [synced_spatial_data_second_half.synced_time.reset_index(drop=True)]
     return first, second, synced_spatial_data_first_half, synced_spatial_data_second_half
 
 
@@ -150,18 +151,33 @@ def process_data(server_path, spike_sorter='/MountainSort', df_path='/DataFrames
     grid_data = all_data[grid_cells]
 
     iterator = 0
-    for index, cell in grid_data.iterrows():
-        first_half, second_half, position_first, position_second = split_in_two(grid_data.iloc[:iterator + 1])
-        iterator += 1
+    corr_coefs_mean = []
+    corr_stds = []
+    for iterator in range(len(grid_data)):
+        print(iterator)
+        print(grid_data.iloc[0].session_id)
+        first_half, second_half, position_first, position_second = split_in_two(grid_data.iloc[iterator:iterator + 1])
         # add rate map to dfs
         # shuffle
         position_heat_map_first, first_half = PostSorting.open_field_firing_maps.make_firing_field_maps(position_first, first_half, prm)
-        spatial_firing_first = OverallAnalysis.shuffle_cell_analysis.shuffle_data(first_half, 20, number_of_times_to_shuffle=1000, animal='mouse', shuffle_type='occupancy')
+        spatial_firing_first = OverallAnalysis.shuffle_cell_analysis.shuffle_data(first_half, 20, number_of_times_to_shuffle=1000, animal='mouse_first_half', shuffle_type='occupancy')
 
         position_heat_map_second, second_half = PostSorting.open_field_firing_maps.make_firing_field_maps(position_second, second_half, prm)
-        spatial_firing_second = OverallAnalysis.shuffle_cell_analysis.shuffle_data(second_half, 20, number_of_times_to_shuffle=1000, animal='mouse', shuffle_type='occupancy')
+        spatial_firing_second = OverallAnalysis.shuffle_cell_analysis.shuffle_data(second_half, 20, number_of_times_to_shuffle=1000, animal='mouse_second_half', shuffle_type='occupancy')
         print('shuffled')
         # compare
+        first_shuffles = spatial_firing_first.shuffled_data[0]
+        second_shuffles = spatial_firing_second.shuffled_data[0]
+        # look at correlations between rows of the two arrays above
+        corr = np.corrcoef(first_shuffles, second_shuffles)[1000:, :1000]
+        corr_mean = corr.mean()
+        corr_std = corr.std()
+
+        corr_coefs_mean.append(corr_mean)
+        corr_stds.append(corr_std)
+
+    #todo print and plot results (corr coefs and std)
+    print('avg corr correlations ')
 
 
 
