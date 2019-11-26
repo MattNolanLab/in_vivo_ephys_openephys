6b9783b85d2949b3392c23c07a1db86f4a8957d7
Teris Tam <teristam@gmail.com>
2019-11-26 22:47:18 +0000

    Bug fix on binary write

--- WriteBinary.py
+++ WriteBinary.py
@@ -24,7 +24,7 @@ def load_OpenEphysRecording(folder,num_tetrodes=setting.num_tetrodes,
     return signal
 
 # %%
-data = load_OpenEphysRecording(debug_folder,4)
+# data = load_OpenEphysRecording(debug_folder,4)
 
 # %% Make some test data
 Fs = 30000
@@ -42,38 +42,41 @@ with open(datapath+'/continuous.dat','wb') as f:
 np.save(datapath+'/timestamps.npy',np.arange(signals.shape[0],dtype='i8'))
 
 #%% Make the structure json file
-structDict = {'GUI version':'0.4.5','continuous':[], 'events':[], 'spikes':[]}
+def writeStructFile(filename,Fs=30000,nChan=16,bit_volts=0.05):
+    structDict = {'GUI version':'0.4.5','continuous':[], 'events':[], 'spikes':[]}
 
-structDict['continuous'] = {
-    "folder_name":"open-ephys/",
-    "sample_rate":Fs,
-    "source_processor_name":"Demo source",
-    "source_processor_id":100,
-    "source_processor_sub_idx":0,
-    "recorded_processor":"Demo source",
-    "recorded_processor_id":100,
-    "num_channels":16,
-    "channels":[]
-}
+    structDict['continuous'] = [{
+        "folder_name":"open-ephys/",
+        "sample_rate":Fs,
+        "source_processor_name":"Demo source",
+        "source_processor_id":100,
+        "source_processor_sub_idx":0,
+        "recorded_processor":"Demo source",
+        "recorded_processor_id":100,
+        "num_channels":16,
+        "channels":[]
+    }]
 
-#assemble channel data
-channels = []
-for i in range(nChan):
-    channels.append({
-          
-        "channel_name":f"CH{i}",
-        "description":"Demo data channel",
-        "identifier":"genericdata.continuous",
-        "history":"Demo source",
-        "bit_volts":0.05,
-        "units":"uV",
-        "source_processor_index":i,
-        "recorded_processor_index":i
+    #assemble channel data
+    channels = []
+    for i in range(nChan):
+        channels.append({
             
-    })
+            "channel_name":f"CH{i}",
+            "description":"Demo data channel",
+            "identifier":"genericdata.continuous",
+            "history":"Demo source",
+            "bit_volts":bit_volts,
+            "units":"uV",
+            "source_processor_index":i,
+            "recorded_processor_index":i
+                
+        })
 
-structDict['continuous']['channels'] = channels
+    structDict['continuous'][0]['channels'] = channels
 
-with open('testData/structure.oebin','w') as f:
-    f.write(json.dumps(structDict,indent=4))
+    with open(filename,'w') as f:
+        f.write(json.dumps(structDict,indent=4))
+
+writeStructFile('testData/structure.oebin')
 # %%
