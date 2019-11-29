831f765207c9efbc334d0532cf66de7de7529ccd
Klara Gerlei <klarizsofi@gmail.com>
2019-11-29 13:37:39 +0000

    print number of spikes on plots

--- OverallAnalysis/shuffle_cell_analysis.py
+++ OverallAnalysis/shuffle_cell_analysis.py
@@ -327,7 +327,7 @@ def plot_bar_chart_for_cells_percentile_error_bar(spatial_firing, path, animal, 
         x_labels = ["0", "", "", "", "", "90", "", "", "", "", "180", "", "", "", "", "270", "", "", "", ""]
         plt.xticks(x_pos, x_labels)
         plt.scatter(x_pos, cell.hd_histogram_real_data_hz, marker='o', color='navy', s=40)
-        plt.title('Number of spikes' + str(cell.number_of_spikes))
+        plt.title('Number of spikes ' + str(cell.number_of_spikes))
         plt.savefig(local_path + 'shuffle_analysis_' + animal + '_' + shuffle_type + str(counter) + str(cell['session_id']) + str(cell['cluster_id']) + '_percentile')
         plt.close()
         counter += 1
