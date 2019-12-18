d86922dab26f263ef3c26b59bb8c8845fae44b73
Klara Gerlei <klarizsofi@gmail.com>
2019-12-18 13:48:59 +0000

    test both possible states of optional param

--- tests/unit/PostSorting/test_heading_direction.py
+++ tests/unit/PostSorting/test_heading_direction.py
@@ -6,7 +6,13 @@ def test_calculate_heading_direction():
     x = [0, 1, 2, 2, 1]
     y = [0, 1, 1, 0, 1]
 
+    desired_result = [45, 45, 0, -90, -45]
+    result = PostSorting.heading_direction.calculate_heading_direction(x, y, pad_fist_value=True)
+
+    assert np.allclose(result, desired_result, rtol=1e-05, atol=1e-08)
+
+
     desired_result = [45, 0, -90, -45]
-    result = PostSorting.heading_direction.calculate_heading_direction(x, y)
+    result = PostSorting.heading_direction.calculate_heading_direction(x, y, pad_fist_value=False)
 
     assert np.allclose(result, desired_result, rtol=1e-05, atol=1e-08)
