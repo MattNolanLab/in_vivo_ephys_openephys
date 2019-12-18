0b31ffac49000cf22d4cf204d7a775aacf301ba0
Klara Gerlei <klarizsofi@gmail.com>
2019-12-18 13:35:32 +0000

    return heading dir in degrees

--- PostSorting/heading_direction.py
+++ PostSorting/heading_direction.py
@@ -2,10 +2,18 @@ import numpy as np
 
 
 def calculate_heading_direction(position_x, position_y):
+
+    '''
+    Calculate heading direction of animal based on the central position of the tracking markers.
+    Method from:
+    https://doi.org/10.1016/j.brainres.2014.10.053
+    '''
+
     delta_x = np.diff(position_x)
     delta_y = np.diff(position_y)
     heading_direction = np.arctan(delta_x / delta_y)
-
+    heading_direction_deg = np.degrees(heading_direction)
+    return heading_direction_deg
 
 
 def add_heading_direction_to_data_frame(spatial_firing):
@@ -13,8 +21,8 @@ def add_heading_direction_to_data_frame(spatial_firing):
 
 
 def main():
-    x = [0, 1, 2, 3, 5, 5]
-    y = [5, 6, 6, 1, 2, 3]
+    x = [0, 1, 2, 2, 1]
+    y = [0, 1, 1, 0, 1]
     calculate_heading_direction(x, y)
 
 
