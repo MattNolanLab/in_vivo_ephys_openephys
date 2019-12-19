df72ff1f7f201c70a3dbb10d4733a1a6751792e8
Klara Gerlei <klarizsofi@gmail.com>
2019-12-19 15:05:01 +0000

    remove space

--- PostSorting/compare_rate_maps.py
+++ PostSorting/compare_rate_maps.py
@@ -45,7 +45,6 @@ def make_same_sized_rate_maps(trajectory_1, trajectory_2, spatial_firing_1, spat
                                                                                   bin_size_pixels, min_dwell,
                                                                                   min_dwell_distance_pixels,
                                                                                   dt_position_ms)
-
     rate_map_2 = PostSorting.open_field_firing_maps.calculate_firing_rate_for_cluster_parallel(cluster, smooth, spatial_firing_2,
                                                                                   trajectory_2.position_x_pixels,
                                                                                   trajectory_2.position_y_pixels,
