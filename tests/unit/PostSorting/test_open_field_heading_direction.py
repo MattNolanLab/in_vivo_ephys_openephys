783f51945310605005fe6363d56a601ef2dc6741
Klara Gerlei <klarizsofi@gmail.com>
2020-01-07 12:49:06 +0000

    Change range in test too.

--- tests/unit/PostSorting/test_open_field_heading_direction.py
+++ tests/unit/PostSorting/test_open_field_heading_direction.py
@@ -6,13 +6,13 @@ def test_calculate_heading_direction():
     x = [0, 1, 2, 2, 1]
     y = [0, 1, 1, 0, 1]
 
-    desired_result = [45, 45, 0, -90, 135]
+    desired_result = [45 + 180, 45 + 180, 0 + 180, -90 + 180, 135 + 180]
     result = PostSorting.open_field_heading_direction.calculate_heading_direction(x, y, pad_first_value=True)
 
     assert np.allclose(result, desired_result, rtol=1e-05, atol=1e-08)
 
 
-    desired_result = [45, 0, -90, 135]
+    desired_result = [45 + 180, 0 + 180, -90 + 180, 135 + 180]
     result = PostSorting.open_field_heading_direction.calculate_heading_direction(x, y, pad_first_value=False)
 
     assert np.allclose(result, desired_result, rtol=1e-05, atol=1e-08)
