267c5d9228855f1cc131b9a41f06f928487f51a3
Klara Gerlei <klarizsofi@gmail.com>
2020-01-31 22:18:40 +0000

    Changes on plots for Figure 2.

--- OverallAnalysis/directional_cell_definition_example_figure.py
+++ OverallAnalysis/directional_cell_definition_example_figure.py
@@ -58,9 +58,41 @@ def plot_bar_chart_for_cells_percentile_error_bar_polar(spatial_firing, path, an
         ax.plot(x_pos, mean, color='grey', linewidth=5, alpha=0.7)
         observed_data = np.append(cell.shuffled_histograms_hz[0], cell.shuffled_histograms_hz[0][0])
         ax.plot(x_pos, observed_data, color='black', linewidth=5, alpha=0.9)
+        plt.ylim(0, max_rate)
         plt.title('\n' + str(max_rate) + ' Hz', fontsize=20, y=1.08)
         plt.subplots_adjust(top=0.85)
         plt.savefig(analysis_path + str(counter) + str(cell['session_id']) + str(cell['cluster_id']) + '_percentile_polar_' + str(cell.percentile_value) + '_polar.png')
+        plt.close()
+        counter += 1
+
+
+def plot_bar_chart_for_cells_percentile_error_bar_polar_observed(spatial_firing, path, animal, shuffle_type='occupancy'):
+    plt.cla()
+    counter = 0
+    for index, cell in spatial_firing.iterrows():
+        mean = np.append(cell['shuffled_means'], cell['shuffled_means'][0])
+        percentile_95 = np.append(cell['error_bar_95'], cell['error_bar_95'][0])
+        percentile_5 = np.append(cell['error_bar_5'], cell['error_bar_5'][0])
+        shuffled_histograms_hz = cell['shuffled_histograms_hz']
+        max_rate = np.round(cell.hd_histogram_real_data_hz.max(), 2)
+        x_pos = np.linspace(0, 2*np.pi, shuffled_histograms_hz.shape[1] + 1)
+        significant_bins_to_mark = np.where(cell.p_values_corrected_bars_bh < 0.05)  # indices
+        significant_bins_to_mark = x_pos[significant_bins_to_mark[0]]
+        y_value_markers = [max_rate + 0.5] * len(significant_bins_to_mark)
+
+        ax = plt.subplot(1, 1, 1, polar=True)
+        ax = plot_utility.style_polar_plot(ax)
+        x_labels = ["0", "", "", "", "", "90", "", "", "", "", "180", "", "", "", "", "270", "", "", "", ""]
+        plt.xticks(x_pos, x_labels)
+        ax.fill_between(x_pos, mean - percentile_5, percentile_95 + mean, color='grey', alpha=0.4)
+        observed_data = np.append(cell.hd_histogram_real_data_hz, cell.hd_histogram_real_data_hz[0])
+        ax.plot(x_pos, observed_data, color='navy', linewidth=7)
+        plt.ylim(0, max_rate)
+        plt.title('\n' + str(max_rate) + ' Hz', fontsize=20, y=1.08)
+        if (cell.p_values_corrected_bars_bh < 0.05).sum() > 0:
+            ax.scatter(significant_bins_to_mark, y_value_markers, c='red', marker='*', zorder=3, s=100)
+        plt.subplots_adjust(top=0.85)
+        plt.savefig(analysis_path + str(counter) + str(cell['session_id']) + str(cell['cluster_id']) + '_percentile_polar_' + str(cell.percentile_value) + '_polar_observed.png')
         plt.close()
         counter += 1
 
@@ -80,10 +112,38 @@ def plot_bar_chart_for_cells_percentile_error_bar(spatial_firing, path, animal, 
             x_labels = ["0", "", "", "", "", "90", "", "", "", "", "180", "", "", "", "", "270", "", "", "", ""]
             plt.xticks(x_pos, x_labels)
             plt.scatter(x_pos, cell.shuffled_histograms_hz[shuffle_example], marker='o', color='grey', s=40)
+            plt.ylim(2, 6.5)
+            plt.yticks([2, 3, 4, 5, 6])
             # plt.title('Number of spikes ' + str(cell.number_of_spikes))
             plt.savefig(analysis_path + 'shuffle_analysis_' + animal + '_' + shuffle_type + str(counter) + str(cell['session_id']) + str(cell['cluster_id']) + '_percentile' + str(shuffle_example))
             plt.close()
             counter += 1
+
+
+def plot_bar_chart_for_cells_percentile_error_bar_observed(spatial_firing, path, animal, shuffle_type='distributive'):
+    for index, cell in spatial_firing.iterrows():
+        mean = cell['shuffled_means']
+        percentile_95 = cell['error_bar_95']
+        percentile_5 = cell['error_bar_5']
+        shuffled_histograms_hz = cell['shuffled_histograms_hz']
+        max_rate = np.round(cell.hd_histogram_real_data_hz.max(), 2)
+        x_pos = np.arange(shuffled_histograms_hz.shape[1])
+        significant_bins_to_mark = np.where(cell.p_values_corrected_bars_bh < 0.05)  # indices
+        significant_bins_to_mark = x_pos[significant_bins_to_mark[0]]
+        y_value_markers = [max_rate + 0.5] * len(significant_bins_to_mark)
+        fig, ax = plt.subplots()
+        ax = OverallAnalysis.shuffle_cell_analysis.format_bar_chart(ax)
+        ax.errorbar(x_pos, mean, yerr=[percentile_5, percentile_95], alpha=0.7, color='black', ecolor='grey', capsize=10, fmt='o', markersize=10)
+        x_labels = ["0", "", "", "", "", "90", "", "", "", "", "180", "", "", "", "", "270", "", "", "", ""]
+        plt.xticks(x_pos, x_labels)
+        plt.scatter(x_pos, cell.hd_histogram_real_data_hz, marker='o', color='navy', s=40)
+        plt.ylim(2, 6.5)
+        plt.yticks([2, 3, 4, 5, 6])
+        if (cell.p_values_corrected_bars_bh < 0.05).sum() > 0:
+            ax.scatter(significant_bins_to_mark, y_value_markers, c='red', marker='*', zorder=3, s=100)
+        # plt.title('Number of spikes ' + str(cell.number_of_spikes))
+        plt.savefig(analysis_path + 'shuffle_analysis_' + animal + '_' + shuffle_type + str(cell['session_id']) + str(cell['cluster_id']) + '_percentile_observed')
+        plt.close()
 
 
 def plot_shuffled_number_of_bins_vs_observed(cell):
@@ -137,7 +197,9 @@ def make_example_plot():
     example_cell = get_number_of_directional_cells(example_cell, tag='grid')
     plot_shuffled_number_of_bins_vs_observed(example_cell)
     plot_bar_chart_for_cells_percentile_error_bar(example_cell, '', 'mouse', shuffle_type='distributive')
+    plot_bar_chart_for_cells_percentile_error_bar_observed(example_cell, '', 'mouse', shuffle_type='distributive')
     plot_bar_chart_for_cells_percentile_error_bar_polar(example_cell, '', 'mouse', shuffle_type='distributive')
+    plot_bar_chart_for_cells_percentile_error_bar_polar_observed(example_cell, '', 'mouse', shuffle_type='distributive')
 
 
 def main():
