26e96ed2f093f86bdafab91cdc2acaf959f334b9
Klara Gerlei <klarizsofi@gmail.com>
2020-02-04 13:36:17 +0000

    Change example cell

--- OverallAnalysis/plot_example_polar_hists_first_and_second_half.py
+++ OverallAnalysis/plot_example_polar_hists_first_and_second_half.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import OverallAnalysis.false_positives
-import OverallAnalysis.analyze_field_correlations
 import PostSorting.open_field_make_plots
 import PostSorting.open_field_head_direction
 
 save_output_path = '/Users/s1466507/Dropbox/Edinburgh/grid_fields/analysis/watson_two_test_cells/'
-local_path = '/Users/s1466507/Dropbox/Edinburgh/grid_fields/analysis/watson_two_test_cells/all_mice_df_2.pkl'
+local_path = '/Users/s1466507/Dropbox/Edinburgh/grid_fields/analysis/watson_two_test_cells/all_mice_df.pkl'
 false_positives_path = '/Users/s1466507/Dropbox/Edinburgh/grid_fields/analysis/watson_two_test_cells/false_positives_all.txt'
 
 
@@ -39,6 +38,7 @@ def plot_hd_for_example_cells(name, cluster):
     hd_hist_position = hd_hist_position * max(hd_hist_cluster)/max(hd_hist_position)
 
     PostSorting.open_field_make_plots.plot_polar_hd_hist(hd_hist_cluster, hd_hist_cluster, cluster, path + '/' + str(cluster), color1='red', color2='red')
+    PostSorting.open_field_make_plots.plot_polar_hd_hist(spatial_firing.iloc[0].hd_hist_first_half, spatial_firing.iloc[0].hd_hist_second_half, 1, save_output_path + 'first_vs_second_half_M14_2018-05-16_11-29-05_of')
 
 
 def main():
@@ -52,17 +52,7 @@ def main():
 
     good_cluster = df_all_mice.false_positive == False
 
-    plot_hd_for_example_cells('M12_2018-04-10_14-22-14_of', 1)
-    plot_hd_for_example_cells('M13_2018-04-27_10-29-02_of', 15)
-
-    first_half_histogram_excitatory = df_all_mice.loc[df_all_mice['session_id'] == 'M12_2018-04-10_14-22-14_of'].hd_hist_first_half[1]
-    second_half_histogram_excitatory = df_all_mice.loc[df_all_mice['session_id'] == 'M12_2018-04-10_14-22-14_of'].hd_hist_second_half[1]
-
-    first_half_histogram_inhibitory = df_all_mice.loc[df_all_mice['session_id'] == 'M13_2018-04-27_10-29-02_of'].hd_hist_first_half[15]
-    second_half_histogram_inhibitory = df_all_mice.loc[df_all_mice['session_id'] == 'M13_2018-04-27_10-29-02_of'].hd_hist_second_half[15]
-
-    PostSorting.open_field_make_plots.plot_polar_hd_hist(first_half_histogram_excitatory, second_half_histogram_excitatory, 1, save_output_path + 'first_vs_second_half_M12_2018-04-03_13-11-02_of')
-    PostSorting.open_field_make_plots.plot_polar_hd_hist(first_half_histogram_inhibitory, second_half_histogram_inhibitory, 15, save_output_path + 'first_vs_second_half_M13_2018-04-27_10-29-02_of')
+    plot_hd_for_example_cells('M14_2018-05-16_11-29-05_of', 12)
 
 
 if __name__ == '__main__':
