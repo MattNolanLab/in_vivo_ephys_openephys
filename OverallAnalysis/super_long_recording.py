1f894cb82e087d1706d5f77f5f32dcaa1021b48a
Klara Gerlei <klarizsofi@gmail.com>
2020-02-11 13:59:23 +0000

    Plot number of significant bars vs shuffled

--- OverallAnalysis/super_long_recording.py
+++ OverallAnalysis/super_long_recording.py
@@ -8,6 +8,7 @@ import pandas as pd
 import PostSorting.parameters
 
 import scipy
+import matplotlib.pylab as plt
 
 
 local_path = OverallAnalysis.folder_path_settings.get_local_path()
@@ -59,6 +60,31 @@ def add_trajectory_data_to_spatial_firing_df(position, spatial_firing):
     return spatial_firing
 
 
+def plot_shuffled_number_of_bins_vs_observed(cell):
+    # percentile = scipy.stats.percentileofscore(cell.number_of_different_bins_shuffled_corrected_p.iloc[0], cell.number_of_different_bins_bh.iloc[0])
+    for index, field in cell.iterrows():
+        shuffled_distribution = cell.number_of_different_bins_shuffled_corrected_p.iloc[index]
+        plt.cla()
+        fig = plt.figure(figsize=(6, 3))
+        plt.yticks([0, 500, 1000])
+        ax = fig.add_subplot(1, 1, 1)
+        ax.set_yticklabels([0, '', 1000])
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.hist(shuffled_distribution, bins=range(20), color='gray')
+        ax.axvline(x=cell.number_of_different_bins_bh.iloc[0], color='navy', linewidth=3)
+        ax.xaxis.set_tick_params(labelsize=20)
+        ax.yaxis.set_tick_params(labelsize=20)
+        # plt.xscale('log')
+        max_x_value = max(cell.number_of_different_bins_bh.iloc[0], shuffled_distribution.max())
+        plt.xlim(0, max_x_value + 1)
+        plt.ylabel('N (shuffles)', fontsize=24)
+        plt.xlabel('N (significant bins)', fontsize=24)
+        plt.tight_layout()
+        plt.savefig(analysis_path + str(index) + 'number_of_significant_bars_shuffled_vs_real_example.png')
+        plt.close()
+
+
 def process_data():
     firing = pd.read_pickle(analysis_path + 'DataFrames/spatial_firing.pkl')
     position = pd.read_pickle(analysis_path + 'DataFrames/position.pkl')
@@ -80,6 +106,7 @@ def process_data():
                  'number_of_spikes_all', 'spatial_scores', 'percentages_of_excluded_bins', 'spatial_scores_field',
                  'percentages_of_excluded_bins_field', 'unsampled_hds']
     aggregated_data_field_correlations = pd.DataFrame(columns=col_names)
+    plot_shuffled_number_of_bins_vs_observed(shuffled_fields)
 
     sampling_rate_video = 30
     for iterator in range(len(shuffled_fields)):
