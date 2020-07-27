63d1ddafcc1afdd172207848e2a3ebd99073a80b
hdclark94 <Worldmap365>
2020-07-27 17:08:25 +0100

    add mean firing rate for local recording (in the cases for which firing rate is needed for post single and concatenated sorting

--- PostSorting/temporal_firing.py
+++ PostSorting/temporal_firing.py
@@ -2,18 +2,35 @@
 def add_temporal_firing_properties_to_df(spatial_firing, prm):
     total_number_of_spikes_per_cluster = []
     mean_firing_rates = []
+    mean_firing_rates_local = []
     for cluster in range(len(spatial_firing)):
         cluster = spatial_firing.cluster_id.values[cluster] - 1
         firing_times = spatial_firing.firing_times[cluster]
         total_number_of_spikes = len(firing_times)
-        total_length_of_recordings = prm.get_total_length_sampling_points()  # this does not include opto
-        mean_firing_rate = total_number_of_spikes / total_length_of_recordings
+        total_length_of_recording = prm.get_total_length_sampling_points()  # this does not include opto
+
+        if prm.stitchpoint is not None:
+            total_length_of_recordings = prm.get_total_length_sampling_points()  # this does not include opto
+            mean_firing_rate = total_number_of_spikes / total_length_of_recordings
+
+            if prm.paired_order == "first":
+                firing_times = firing_times[firing_times > 0]
+            elif prm.paired_order == "second":
+                firing_times = firing_times[firing_times < prm.stitchpoint]
+
+            total_number_of_spikes_local = len(firing_times)
+            mean_firing_rate_local = total_number_of_spikes_local/total_length_of_recording
+            mean_firing_rates_local.append(mean_firing_rate_local)
+        else:
+            mean_firing_rate = total_number_of_spikes / total_length_of_recording  # this does not include opto
 
         total_number_of_spikes_per_cluster.append(total_number_of_spikes)
         mean_firing_rates.append(mean_firing_rate)
 
     spatial_firing['number_of_spikes'] = total_number_of_spikes_per_cluster
     spatial_firing['mean_firing_rate'] = mean_firing_rates
+    if prm.stitchpoint is not None:
+        spatial_firing['mean_firing_rate_local'] = mean_firing_rates_local
 
     return spatial_firing
 
