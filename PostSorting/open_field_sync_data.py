b80c91758ad1d7e4718b915588496f962601ceca
hdclark94 <Worldmap365>
2020-07-27 17:24:20 +0100

    total_length is set regardless of prm.stitchpoint

--- PostSorting/open_field_sync_data.py
+++ PostSorting/open_field_sync_data.py
@@ -195,7 +195,6 @@ def process_sync_data(recording_to_process, prm, spatial_data):
     synced_spatial_data = synced_spatial_data.reset_index(drop=True)
 
     synced_spatial_data = remove_opto_tagging_from_spatial_data(prm, synced_spatial_data)
-    if prm.stitchpoint is None:
-        prm.set_total_length_sampling_points(synced_spatial_data.synced_time.values[-1]) # seconds
+    prm.set_total_length_sampling_points(synced_spatial_data.synced_time.values[-1]) # seconds
 
     return synced_spatial_data, is_found
