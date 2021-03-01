2600d9d06435014a180f126e266a944ced9e954c
Klara Gerlei <klarizsofi@gmail.com>
2021-03-01 12:06:47 +0000

    change order

--- PostSorting/open_field_light_data.py
+++ PostSorting/open_field_light_data.py
@@ -181,10 +181,10 @@ def analyse_latencies(spatial_firing, prm):
 def process_spikes_around_light(spatial_firing, prm, window_size_ms=40):
     print('I will process opto data.')
     on_pulses, window_size_sampling_rate = get_peristumulus_opto_data(window_size_ms, prm)
-    peristimulus_spikes = make_peristimulus_df(spatial_firing, on_pulses, window_size_sampling_rate, prm)
     spatial_firing = add_first_spike_times_after_stimulation(spatial_firing, on_pulses)
     spatial_firing = analyse_latencies(spatial_firing, prm)
     spatial_firing = PostSorting.load_snippet_data_opto.get_opto_snippets(spatial_firing, prm, random_snippets=True, column_name='first_spike_snippets_opto', firing_times_column='spike_times_after_opto')
+    peristimulus_spikes = make_peristimulus_df(spatial_firing, on_pulses, window_size_sampling_rate, prm)
     # plt.plot((peristimulus_spikes.iloc[:, 2:].astype(int)).sum().rolling(50).sum())
     # baseline, test = create_baseline_and_test_epochs(peristimulus_spikes)
     # latencies, p_values, I_values = salt(baseline_trials, test_trials, winsize=0.01 * pq.s, latency_step=0.01 * pq.s)
