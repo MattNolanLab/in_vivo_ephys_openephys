7c23fb2f3a36c2c874d39c7bf3d2cf56df9dd845
Teris Tam <teristam@gmail.com>
2021-03-02 23:23:24 +0000

    Change oe threshold to 1

--- PostSorting/open_field_sync_data.py
+++ PostSorting/open_field_sync_data.py
@@ -139,7 +139,7 @@ def get_synchronized_spatial_data(sync_data_ephys, spatial_data, prm):
     bonsai = spatial_data['syncLED'].values
     oe = sync_data_ephys_downsampled.sync_pulse.values
     bonsai = reduce_noise(bonsai, np.median(bonsai) + 4 * np.std(bonsai))
-    oe = reduce_noise(oe, 0.01)
+    oe = reduce_noise(oe, 1)
     bonsai, oe = pad_shorter_array_with_0s(bonsai, oe)
     corr = np.correlate(bonsai, oe, "full")  # this is the correlation array between the sync pulse series
 
