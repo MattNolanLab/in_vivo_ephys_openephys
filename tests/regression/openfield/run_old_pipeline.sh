29b6a1219a4337e29d217677f7608c246e173af9
Teris Tam <teristam@gmail.com>
2021-03-03 12:18:42 +0000

    fix bug in data path

--- tests/regression/openfield/run_old_pipeline.sh
+++ tests/regression/openfield/run_old_pipeline.sh
@@ -1,7 +1,7 @@
-tar xvf ~/testdata/M1_D31_short.tar.gz -C ~/testdata
+tar xvf ~/testdata/M5_2018-03-06_15-34-44_of.tar.gz -C ~/testdata
 docker run -it -v ${PWD}/regression:/code \
     -v ~/testdata:/home/nolanlab/to_sort/recordings \
-    -v ${PWD}/tests/regression/output/openfield:/ActiveProjects/Sarah/Data/PIProject_OptoEphys/Data/OpenEphys/_cohort3/VirtualReality/M1_D31_2018-11-01_12-28-25 \
+    -v ${PWD}/tests/regression/output/openfield:/ActiveProjects/Klara/Open_field_opto_tagging_p038/M5_2018-03-06_15-34-44_of/ \
     -v ${PWD}/regression/sorting_files:/home/nolanlab/to_sort/sorting_files \
     -e SERVER_PATH_FIRST_HALF='/ActiveProjects/' \
     -e DEBUG=1 \
