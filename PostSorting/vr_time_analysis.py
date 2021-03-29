0169648897421818c8891886dbda73dd78e48ed9
hdclark94 <Worldmap365>
2021-03-29 14:01:52 +0100

    renamed time_trials_binned to times_binned
    removed time_trial_numbers, time_trial_types

--- PostSorting/vr_time_analysis.py
+++ PostSorting/vr_time_analysis.py
@@ -8,27 +8,17 @@ def calculate_binned_time(raw_position_data,processed_position_data, prm):
     numbers_of_bins = get_number_of_bins(prm)
     bin_size_cm = get_bin_size(prm, numbers_of_bins)
 
-    time_trials_binned = []
-    time_trial_numbers = []
-    time_trialtypes = []
-
+    times_binned = []
     for trial_number in range(1, max(raw_position_data["trial_number"]+1)):
-        trial_type = np.array(raw_position_data['trial_type'][np.array(raw_position_data['trial_number']) == trial_number])[0]
-
         trial_x_position_cm = np.array(raw_position_data['x_position_cm'][np.array(raw_position_data['trial_number']) == trial_number])
         trial_times = np.array(raw_position_data['dwell_time_ms'][np.array(raw_position_data['trial_number']) == trial_number])
 
         bins = np.arange(0, prm.get_track_length(), bin_size_cm)
         bin_times = np.histogram(trial_x_position_cm, bins, weights=trial_times)[0]
 
-        time_trials_binned.append(bin_times)
-        time_trial_numbers.append(trial_number)
-        time_trialtypes.append(trial_type)
+        times_binned.append(bin_times)
 
-    processed_position_data['time_trials_binned'] = pd.Series(time_trials_binned)
-    processed_position_data['time_trial_numbers'] = pd.Series(time_trial_numbers)
-    processed_position_data['time_trial_types'] = pd.Series(time_trialtypes)
-
+    processed_position_data['times_binned'] = times_binned
     return processed_position_data
 
 def get_bin_size(prm, numbers_of_bins):
