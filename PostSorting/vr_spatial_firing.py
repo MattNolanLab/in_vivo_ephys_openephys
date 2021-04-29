5d2f3be7884bd02b7068f7c41f7f2dd2a6be2342
hdclark94 <Worldmap365>
2021-04-29 11:45:58 +0100

    fix bug in time binning

--- PostSorting/vr_spatial_firing.py
+++ PostSorting/vr_spatial_firing.py
@@ -56,11 +56,8 @@ def bin_fr_in_time(spike_data, raw_position_data):
     fr_binned_in_time = [[] for x in range(len(spike_data))]
 
     for trial_number in range(1, max(raw_position_data["trial_number"]+1)):
-        trial_times = np.array(raw_position_data['time_seconds'][np.array(raw_position_data['trial_number']) == trial_number])
+        trial_times = np.array(raw_position_data['time_seconds'][np.array(raw_position_data['trial_number']) == trial_number], dtype="float64")
         time_bins = np.arange(min(trial_times), max(trial_times), settings.time_bin_size)# 100ms time bins
-
-        # get the index of the first time point in the trial so we can subtract it from our spike indexes for each trial
-        i_start = np.where(np.array(raw_position_data['trial_number']) == trial_number)[0][0]
 
         for i, cluster_id in enumerate(spike_data.cluster_id):
             if len(time_bins)>1:
@@ -68,8 +65,8 @@ def bin_fr_in_time(spike_data, raw_position_data):
                 trial_numbers = np.array(spike_data[spike_data["cluster_id"] == cluster_id]["trial_number"].iloc[0])
                 trial_firing_times = firing_times[trial_numbers == trial_number]
 
-                # count the spikes in each time bin
-                fr_hist = np.histogram((trial_firing_times-i_start)/settings.sampling_rate, time_bins)[0]/settings.time_bin_size
+                # convert spike indices to spike times, count the spikes in each time bin and divide by time bin size to get fr
+                fr_hist = np.histogram(trial_firing_times/settings.sampling_rate, time_bins)[0]/settings.time_bin_size
 
                 # and smooth
                 fr_hist = convolve(fr_hist, gauss_kernel)
@@ -93,11 +90,8 @@ def bin_fr_in_space(spike_data, raw_position_data):
 
     for trial_number in range(1, max(raw_position_data["trial_number"]+1)):
         trial_x_position_cm = np.array(raw_position_data['x_position_cm'][np.array(raw_position_data['trial_number']) == trial_number], dtype="float64")
-        trial_x_dwell_time = np.array(raw_position_data['dwell_time_ms'][np.array(raw_position_data['trial_number']) == trial_number])
+        trial_x_dwell_time = np.array(raw_position_data['dwell_time_ms'][np.array(raw_position_data['trial_number']) == trial_number], dtype="float64")
         pos_bins = np.arange(min(trial_x_position_cm), max(trial_x_position_cm), settings.vr_bin_size_cm)# 100ms time bins
-
-        # get the index of the first time point in the trial so we can subtract it from our spike indexes for each trial
-        i_start = np.where(np.array(raw_position_data['trial_number']) == trial_number)[0][0]
 
         for i, cluster_id in enumerate(spike_data.cluster_id):
             if len(pos_bins)>1:
@@ -107,12 +101,13 @@ def bin_fr_in_space(spike_data, raw_position_data):
 
                 # count the spikes in each space bin and normalise by the total time spent in that bin for the trial
                 fr_hist, bin_edges = np.histogram(trial_firing_locations, pos_bins)
-                fr_hist = fr_hist/np.histogram(trial_x_position_cm, pos_bins, weights=trial_x_dwell_time)[0]
+                fr_hist = fr_hist/(np.histogram(trial_x_position_cm, pos_bins, weights=trial_x_dwell_time)[0])
 
                 # get location bin centres
                 bin_centres = 0.5*(bin_edges[1:]+bin_edges[:-1])
 
-                # and smooth
+                # nans to zero and smooth
+                fr_hist[np.isnan(fr_hist)] = 0
                 fr_hist = convolve(fr_hist, gauss_kernel)
 
                 # append the binned firing for each cluster at each trial
