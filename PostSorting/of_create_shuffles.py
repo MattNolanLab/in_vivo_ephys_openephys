07dd5912fc95f0f0730e2e7b1fdc2164daa64795
hdclark94 <Worldmap365>
2021-06-09 13:11:08 +0100

    add a if statement for only submitting jobs on recordings without a comppleted shuffle

--- PostSorting/of_create_shuffles.py
+++ PostSorting/of_create_shuffles.py
@@ -18,15 +18,18 @@ subprocess.check_call(f'rsync -avP --include "*/" --include="*.pkl" --exclude="*
 recordings_scratch_path = local_scratch_path+"/"+recording_folder_to_process.split("/")[-1]
 recording_list = [f.path for f in os.scandir(recordings_scratch_path) if f.is_dir()]
 
-for recording_name in recording_list:
-    print(f'Copying spatial_firing.pkl and position.pkl from Datastore (via Eleanor) to Eddie Scratch')
-    remote_recording_path = ELEANOR_RECORDINGS_PATH / recording_name
-    local_recording_path = LOCAL_SCRATCH_PATH / recording_name
+for recording_name in recording_list: # eg. M1_D1_2020-01-31_00-00-00
+    if os.path.isfile(recordings_scratch_path+"/"+recording_name):
+        print("shuffle.pkl was found for ", recording_name)
+    else:
+        print(f'Copying spatial_firing.pkl and position.pkl from Datastore (via Eleanor) to Eddie Scratch')
+        remote_recording_path = ELEANOR_RECORDINGS_PATH / recording_name
+        local_recording_path = LOCAL_SCRATCH_PATH / recording_name
 
-    print(f'Recordings copied... Submitting shuffle jobs to Eddie')
-    for shuffle_number in range(N_SHUFFLES):
-        print(f'Submitting shuffle job {shuffle_number}')
-        cmd = f'qsub -v RECORDING_PATH={local_recording_path} -v SHUFFLE_NUMBER={shuffle_number} /home/s1228823/in_vivo_ephys_openephys/PostSorting/run_of_shuffle.sh'
-        subprocess.check_call(cmd, shell=True)
+        print(f'Recordings copied... Submitting shuffle jobs to Eddie')
+        for shuffle_number in range(N_SHUFFLES):
+            print(f'Submitting shuffle job {shuffle_number}')
+            cmd = f'qsub -v RECORDING_PATH={local_recording_path} -v SHUFFLE_NUMBER={shuffle_number} /home/s1228823/in_vivo_ephys_openephys/PostSorting/run_of_shuffle.sh'
+            subprocess.check_call(cmd, shell=True)
 
 print("all jobs submitted, good luck!")
