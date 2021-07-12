8a8f43964427b0453c01f71d4f08c98490c9a226
hdclark94 <Worldmap365>
2021-07-12 12:12:43 +0100

    test pathing

--- PostSorting/OpenFieldShuffleAnalysis/of_create_shuffles.py
+++ PostSorting/OpenFieldShuffleAnalysis/of_create_shuffles.py
@@ -26,9 +26,10 @@ for recording_name in recording_list: # eg. M1_D1_2020-01-31_00-00-00
     remote_recording_path = ELEANOR_RECORDINGS_PATH / recording_name
     local_recording_path = LOCAL_SCRATCH_PATH / recording_name
 
+    print(local_scratch_path + "/" + recording_name + "/MountainSort/DataFrames/shuffles/shuffle.pkl")
+
     if os.path.isfile(local_scratch_path + "/" + recording_name + "/MountainSort/DataFrames/shuffles/shuffle.pkl"):
         print("shuffle dataframe found, I will only submit a job if more shuffles are required")
-        print(local_scratch_path + "/" + recording_name + "/MountainSort/DataFrames/shuffles/shuffle.pkl")
         shuffle = pd.read_pickle(local_scratch_path + "/" + recording_name + "/MountainSort/DataFrames/shuffles/shuffle.pkl")
 
         #only submit a job if the shuffle dataframe is incomplete
