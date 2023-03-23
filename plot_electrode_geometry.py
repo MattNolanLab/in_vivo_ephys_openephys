551efd0fdaa22882d4ac54e891c2cbf96542d0b7
hclark94 <harrydclark91@gmail.com>
2023-03-23 11:12:27 +0000

    add function for plotting electrode geometry for sanity checks

--- plot_electrode_geometry.py
+++ plot_electrode_geometry.py
@@ -15,7 +15,18 @@ def test_probe_interface(save_path):
     fig = plt.figure(figsize=(6,6))
     ax = fig.add_subplot(1, 1, 1)
     plot_probe(recording_probes.get_probe(), ax=ax, with_channel_index=True)
-    plt.savefig("/mnt/datastore/Harry/test_recording/probe_locations_"+str(64)+"_channels.png", dpi=200)
+    plt.savefig("/mnt/datastore/Harry/test_recording/probe_locations_"+str(64)+"_channels.png", dpi=600)
+    plt.close()
+
+    recording, sorting_true = se.toy_example(duration=1, num_channels=64, seed=0, num_segments=4)
+    probe1 = get_probe('cambridgeneurotech', 'ASSY-236-P-1')
+    probe1.set_device_channel_indices(np.arange(64))
+    print(probe1)
+    recording_probes = recording.set_probe(probe1, group_mode='by_shank')
+    fig = plt.figure(figsize=(6,6))
+    ax = fig.add_subplot(1, 1, 1)
+    plot_probe(recording_probes.get_probe(), ax=ax, with_device_index=True)
+    plt.savefig("/mnt/datastore/Harry/test_recording/probe_locations_"+str(64)+"_channels_plus_channel_indices.png", dpi=600)
     plt.close()
 
 def test_double_probe_interface(save_path):
@@ -33,8 +44,8 @@ def test_double_probe_interface(save_path):
     recording_probes = recording.set_probegroup(probegroup, group_mode='by_shank')
     fig = plt.figure(figsize=(6,6))
     ax = fig.add_subplot(1, 1, 1)
-    plot_probe_group(recording_probes.get_probegroup(), ax=ax)
-    plt.savefig("/mnt/datastore/Harry/test_recording/probe_locations_"+str(128)+"_channels.png", dpi=200)
+    plot_probe_group(recording_probes.get_probegroup(), ax=ax, with_channel_index=True, with_device_index=True)
+    plt.savefig("/mnt/datastore/Harry/test_recording/probe_locations_"+str(128)+"_channels.png", dpi=600)
     plt.close()
 
     probe_df = probegroup.to_dataframe()
@@ -44,7 +55,7 @@ def main():
     print('-------------------------------------------------------------')
     print('-------------------------------------------------------------')
     save_path = "/mnt/datastore/Harry/test_recording/"
-    #test_probe_interface(save_path)
+    test_probe_interface(save_path)
     test_double_probe_interface(save_path)
     print("look now")
 
